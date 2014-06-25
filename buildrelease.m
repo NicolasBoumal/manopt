b82be167658899306345cccf1b00e464bb41ad72
unknown <nboumal@PC-220-744.oasis.uclouvain.be>
2014-06-25 14:51:49 +0200

    Created a script called buildrelease.m to aid with new releases.

---
+++ buildrelease.m
@@ -0,0 +1,61 @@
+% Attempt at a script to facilitate the generation of a new release of
+% manopt.
+
+fprintf('Release? Are your sure? CTRL+C if not. Any other key if yes.\n');
+pause();
+
+version_str = input('What version is this? (between quotes. example: ''1.0.42'') ');
+
+% This has to be done manually for now.
+fprintf('You must now manually edit these files for version names and date:\n');
+fprintf('README.txt and manopt_version.m.\n');
+fprintf('Today is: %s\n', date());
+fprintf('Press any key AFTER you are done.');
+edit auxiliaries/manopt_version.m;
+edit auxiliaries/README.txt;
+pause();
+
+cd releases;
+rep = sprintf('Manopt_%s', version_str);
+mkdir(rep);
+
+cd(rep);
+mkdir manopt;
+cd ..;
+cd ..;
+
+% We are now back at the root of the manopt repository
+
+% Copy everything that's needed for a release.
+
+targetrep = ['releases/', rep, '/manopt/'];
+
+copyfile('importmanopt.m', targetrep, 'f');
+copyfile('examples', [targetrep, 'examples/'], 'f');
+copyfile('manopt', [targetrep, 'manopt/'], 'f');
+copyfile('auxiliaries', targetrep, 'f');
+
+% Let's go and erase all *.asv files
+cd(targetrep);
+dirlist = strread(genpath(pwd()), '%s', 'delimiter', ';');
+for subdir = dirlist'
+    delete([subdir{1}, '/*.asv']);
+end
+cd ..;
+cd ..;
+cd ..;
+
+cd releases;
+cd(rep);
+zip(rep, '*');
+cd ..;
+cd ..;
+
+% A heads up for reference generation
+fprintf('About to generate the reference for the website. Press any key.\n');
+pause();
+cd reference;
+generate_manopt_reference;
+cd ..;
+
+fprintf('Now: update the website !\n');
