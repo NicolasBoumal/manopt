70bebd52e174c46521371ffac274a007642bdac7
Nicolas Boumal <nicolasboumal@gmail.com>
2014-07-17 15:46:43 +0200

    The low rank matrix completion example now illustrates the use of a home made linesearch helper (the very new feature of Manopt) and the tutorial page links to that example file.

--- examples/low_rank_matrix_completion.m
+++ examples/low_rank_matrix_completion.m
@@ -9,9 +9,12 @@ function low_rank_matrix_completion()
 % "Low-rank matrix completion by Riemannian optimization"
 % Bart Vandereycken - SIAM Journal on Optimization, 2013.
 %
-% This can be a starting point for many optimization problems of the form
+% This can be a starting point for many optimization problems of the form:
 %
 % minimize f(X) such that rank(X) = k, size(X) = [m, n].
+%
+% Note that the code is long because it showcases quite a few features of
+% Manopt: most of the code is optional.
 %
 % Input:  None. This example file generate random data.
 % 
@@ -25,8 +28,8 @@ function low_rank_matrix_completion()
 % Change log:
 % 
     
-    % Choose the size of the problem:
-    % We will complete a matrix of size mxn of rank k.
+    % Random data generation. First, choose the size of the problem.
+    % We will complete a matrix of size mxn of rank k:
     m = 200;
     n = 500;
     k = 10;
@@ -40,6 +43,12 @@ function low_rank_matrix_completion()
     P = sparse(rand(m, n) <= fraction);
     % Hence, we know the nonzero entries in PA:
     PA = P.*A;
+    
+    
+    
+    
+    
+    
     
     % Pick the manifold of matrices of size mxn of fixed rank k.
     problem.M = fixedrankembeddedfactory(m, n, k);
@@ -67,10 +76,11 @@ function low_rank_matrix_completion()
         G = P.*Xmat - PA;
     end
 
+    % This is optional, but it's nice if you have it.
     % Define the Euclidean Hessian of the cost at X, along H, where H is
-    % represented as a tangent vector.
+    % represented as a tangent vector: a structure with fields Up, Vp, M.
+    % This is the directional derivative of nabla f(X) at X along Xdot:
     % nabla^2 f(X)[Xdot] = P.*Xdot
-    % (This is the directional derivative of nabla f(X) at X along Xdot.)
     problem.ehess = @euclidean_hessian;
     function ehess = euclidean_hessian(X, H)
         % The function tangent2ambient transforms H (a tangent vector) into
@@ -89,12 +99,18 @@ function low_rank_matrix_completion()
     end
     
 
+
+
     % Check consistency of the gradient and the Hessian. Useful if you
-    % adapt this example for a now cost function and you would like to make
+    % adapt this example for a new cost function and you would like to make
     % sure there is no mistake.
     % warning('off', 'manopt:fixedrankembeddedfactory:exp');
     % checkgradient(problem); pause;
     % checkhessian(problem); pause;
+    
+    
+    
+    
     
     % Compute an initial guess. Points on the manifold are represented as
     % structures with three fields: U, S and V. U and V need to be
@@ -112,14 +128,102 @@ function low_rank_matrix_completion()
     % U, S and V.
     Xmat = X.U*X.S*X.V';
     fprintf('||X-A||_F = %g\n', norm(Xmat - A, 'fro'));
+    
+    
+    
+    
+    
+    
+    % Alternatively, we could decide to use a solver such as
+    % steepestdescent or conjugategradient. These solvers need to solve a
+    % line-search problem at each iteration. Standard line searches in
+    % Manopt have generic purpose systems to do this. But for the problem
+    % at hand, it so happens that we can rather accurately guess how far
+    % the line-search should look, and it would be a waste to not use that.
+    % Look up the paper referenced above for the mathematical explanation
+    % of the code below.
+    % 
+    % To tell Manopt about this special information, we specify the
+    % linesearch hint function in the problem structure. Notice that this
+    % is not the same thing as specifying a linesearch function in the
+    % options structure.
+    % 
+    % Both the SD and the CG solvers will detect that we
+    % specify the hint function below, and they will use an appropriate
+    % linesearch algorithm by default, as a result. Typically, they will
+    % try the step t*H first, then if it does not satisfy an Armijo
+    % criterion, they will decrease t geometrically until satisfaction or
+    % failure.
+    % 
+    % Just like the cost, egrad and ehess functions, the linesearch
+    % function could use a store structure if you like. The present code
+    % does not use the store structure, which means quite a bit of the
+    % computations are made redundantly, and as a result a better method
+    % could appear slower. See the Manopt tutorial about caching when you
+    % are ready to switch from a proof-of-concept code to an efficient
+    % code.
+    %
+    % The inputs are X (a point on the manifold) and H, a tangent vector at
+    % X that is assumed to be a descent direction. That is, there exists a
+    % positive t such that f(Retraction_X(tH)) < f(X). The function below
+    % is supposed to output a "t" that it is a good "guess" at such a t.
+    problem.linesearch = @linesearch_helper;
+    function t = linesearch_helper(X, H)
+        % Note that you would not usually need the Hessian for this.
+        residual_omega = nonzeros(problem.egrad(X));
+        dir_omega      = nonzeros(problem.ehess(X, H));
+        t = - dir_omega \ residual_omega ;
+    end
+
+    % Notice that for this solver, the Hessian is not needed.
+    [Xcg, xcost, info, options] = conjugategradient(problem, X0);
+    
+    fprintf('Take a look at the options that CG used:\n');
+    disp(options);
+    fprintf('And see how many trials were made at each line search call:\n');
+    info_ls = [info.linesearch];
+    disp([info_ls.costevals]);
+
+    
+    
+    
+    
+    
+    
+    
+    fprintf('Try it again without the linesearch helper.\n');
+    
+    % Remove the linesearch helper from the problem structure.
+    problem = rmfield(problem, 'linesearch');
+    
+    [Xcg, xcost, info, options] = conjugategradient(problem, X0);
+    
+    fprintf('Take a look at the options that CG used:\n');
+    disp(options);
+    fprintf('And see how many trials were made at each line search call:\n');
+    info_ls = [info.linesearch];
+    disp([info_ls.costevals]);
+    
+    
+    
+    
+    
+    
 
     % If the problem has a small enough dimension, we may (for analysis
     % purposes) compute the spectrum of the Hessian at a point X. This may
-    % help in studying the conditioning of a problem.
+    % help in studying the conditioning of a problem. If you don't provide
+    % the Hessian, Manopt will approximate the Hessian with finite
+    % differences of the gradient and try to estimate its "spectrum" (it's
+    % not a proper linear operator). This can give some intuition, but
+    % should not be relied upon.
     if problem.M.dim() < 100
         fprintf('Computing the spectrum of the Hessian...');
         s = hessianspectrum(problem, X);
         hist(s);
     end
+    
+    
+    
     
 end
