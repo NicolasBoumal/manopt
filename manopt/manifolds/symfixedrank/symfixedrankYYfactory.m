ee8e589196ffd48987887d5136262c25b0e4f068
Nicolas Boumal <nicolasboumal@gmail.com>
2014-08-12 10:34:39 +0200

    Typos in comments and better comments.

--- manopt/manifolds/symfixedrank/symfixedrankYYfactory.m
+++ manopt/manifolds/symfixedrank/symfixedrankYYfactory.m
@@ -5,21 +5,32 @@ function M = symfixedrankYYfactory(n, k)
 %
 % The geometry is based on the paper,
 % M. Journee, P.-A. Absil, F. Bach and R. Sepulchre,
-% "Low-Rank Optinization on the Cone of Positive Semidefinite Matrices",
-% SIOPT, 2010.
+% "Low-Rank Optimization on the Cone of Positive Semidefinite Matrices",
+% SIAM Journal on Optimization, 2010.
 %
 % Paper link: http://www.di.ens.fr/~fbach/journee2010_sdp.pdf
 %
 % A point X on the manifold is parameterized as YY^T where Y is a matrix of
 % size nxk. The matrix Y (nxk) is a full column-rank matrix. Hence, we deal 
 % directly with Y.
+%
+% Notice that this manifold is not complete: if optimization leads Y to be
+% rank-deficient, the geometry will break down. Hence, this geometry should
+% only be used if it is expected that the points of interest will have rank
+% exactly k. Reduce k if that is not the case.
+% 
+% An alternative, complete, geometry for positive semidefinite matrices of
+% rank k is described in Bonnabel and Sepulchre 2009, "Riemannian Metric
+% and Geometric Mean for Positive Semidefinite Matrices of Fixed Rank",
+% SIAM Journal on Matrix Analysis and Applications.
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Bamdev Mishra, Dec. 30, 2012.
 % Contributors:
 % Change log:
-%  July 10, 2013 (NB) : added vec, mat, tangent, tangent2ambient ;
-%                       correction for the dimension of the manifold.
+%  July 10, 2013 (NB)
+%       Added vec, mat, tangent, tangent2ambient ;
+%       Correction for the dimension of the manifold.
 
 
 M.name = @() sprintf('YY'' quotient manifold of %dx%d PSD matrices of rank %d', n, k);
@@ -31,7 +42,7 @@ M.inner = @(Y, eta, zeta) trace(eta'*zeta);
 
 M.norm = @(Y, eta) sqrt(M.inner(Y, eta, eta));
 
-M.dist = @(Y, Z) error('symfixedrankYYfactory.dist not implenented yet.');
+M.dist = @(Y, Z) error('symfixedrankYYfactory.dist not implemented yet.');
 
 M.typicaldist = @() 10*k;
 
