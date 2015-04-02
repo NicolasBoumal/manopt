575c606a03ddc58129b53f5abb9205d368cef3e3
Nicolas Boumal <nicolasboumal@gmail.com>
2015-04-02 18:46:33 +0200

    dbstore managment functions (getStore, setStore, purgeStoredb) now handle an extre, special field: permanent.

--- manopt/privatetools/getStore.m
+++ manopt/privatetools/getStore.m
@@ -7,22 +7,42 @@ function store = getStore(problem, x, storedb)
 % returns the store structure corresponding to the point x. If there is no
 % record for the point x, returns an empty structure.
 %
+% That structure is then complemented with a field called 'permanent'. That
+% field can also be modified, and will be the same regardless of x. In
+% other words: it makes it possible to store and pass information around
+% between different points.
+%
 % See also: setStore purgeStoredb
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, Dec. 30, 2012.
 % Contributors: 
-% Change log: 
+% Change log:
+%
+%   April 2, 2015, NB:
+%       storedb may now contain a special field, called 'permanent', which
+%       is then added to the returned store. This special field is not
+%       associated to a specific store; it is passed around from call to
+%       call, to create a 'permanent memory' that spans, for example, all
+%       iterations of a solver's execution.
    
     % Construct the fieldname (key) associated to the queried point x.
     key = problem.M.hash(x);
     
-    % If there is a value stored for this key, return it.
-    % Otherwise, return an empty structure.
+    % If there is a value stored for this key, extract it.
+    % Otherwise, create an empty structure.
     if isfield(storedb, key)
         store = storedb.(key);
     else
         store = struct();
+    end
+    
+    % If there is a permanent memory, add it.
+    % Otherwise, add an empty structure.
+    if isfield(storedb, 'permanent')
+        store.permanent = storedb.permanent;
+    else
+        store.permanent = struct();
     end
 
 end
--- manopt/privatetools/setStore.m
+++ manopt/privatetools/setStore.m
@@ -8,14 +8,26 @@ function storedb = setStore(problem, x, storedb, store)
 % record for the point x, it is created and set to store. The updated
 % storedb database is returned. The lastset__ field of the store structure
 % keeps track of which stores were updated latest.
+%
+% If store contains a field called "permanent", its contents are placed in
+% storedb.permanent, and the field is removed from the store structre
+% before storage.
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, Dec. 30, 2012.
 % Contributors: 
 % Change log: 
+%
 %   Dec. 6, 2013, NB:
 %       Now using a persistent uint32 counter instead of cputime to track
 %       the most recently modified stores.
+%
+%   April 2, 2015, NB:
+%       storedb may now contain a special field, called 'permanent', which
+%       is updated from the given store. This special field is not
+%       associated to a specific store; it is passed around from call to
+%       call, to create a 'permanent memory' that spans, for example, all
+%       iterations of a solver's execution.
 
     % This persistent counter is used to keep track of the order in which
     % store structures are updated. This is used by purgeStoredb to erase
@@ -29,14 +41,23 @@ function storedb = setStore(problem, x, storedb, store)
     assert(nargout == 1, ...
            'The output of setStore should replace your storedb.');
    
+    % If there is a permanent memory, extract it.
+    if isfield(store, 'permanent')
+        storedb.permanent = store.permanent;
+        % Removing a field appears to be rather slow.
+        % store = rmfield(store, 'permanent');
+        % Instead, we simply empty the contents:
+        store.permanent = struct();
+    end
+    
+    % Add / update a last-set flag.
+    store.lastset__ = counter;
+    counter = counter + 1;
+       
     % Construct the fieldname (key) associated to the current point x.
     key = problem.M.hash(x);
     
-    % Set the value associated to that key to store.
+    % Put the store in storage at that key.
     storedb.(key) = store;
-    
-    % Add / update a last-set flag.
-    storedb.(key).lastset__ = counter;
-    counter = counter + 1;
 
 end
