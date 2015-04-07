c2eb8dc379417c09c4bd129a0030271e070ea772
BM <bamdevm@gmail.com>
2015-04-07 15:23:43 +0100

    Distance completion with proper documentation.

--- tests/lowrank_dist_completion.m
+++ tests/lowrank_dist_completion.m
@@ -1,71 +1,296 @@
-function lowrank_dist_completion( )
-% lowrank_dist_completion()
-% Low-rank Euclidean distance matrix completion
+function [Y, out_infos, out_problem_description] =  lowrank_dist_completion(problem_description)
+% Perform low-rank distance completion w/ automatic rank detection.
 %
+% function Y =  lowrank_dist_completion(problem_description)
+% function [Y, infos, out_problem_description] =  lowrank_dist_completion(problem_description)
+%
+% It implements the ideas of Journee, Bach, Absil and Sepulchre, SIOPT,
+% 2010 applied to the problem of low-rank distance completion. The details
+% are in the paper "Low-rank optimization for distance matrix completion",
+% B. Mishra, G. Meyer, and R. Sepulchre, IEEE CDC, 2011.
+%
+% Paper link: http://arxiv.org/abs/1304.6663.
+%
+% Inputs:
+% -------
+%
+% problem_description: The problem structure with the description of the problem.
+%
+%
+% - problem_description.data_train: Data structure for known distances that are used to learn a low-rank model.
+%                                   It contains the 3 fields that are shown
+%                                   below. An empty "data_train" structure
+%                                   will generate a random problem instance
+%
+%       -- data_train.entries:      A column vector consisting of known
+%                                   distances. An empty "data_train.entries"
+%                                   field will generate a random
+%                                   problem instance..
+%
+%       -- data_train.rows:         The row position of th corresponding
+%                                   distances. An empty "data_train.rows"
+%                                   field will generate a random
+%                                   problem instance.
+%
+%       -- data_train.cols:         The column position of th corresponding
+%                                   distances. An empty "data_train.cols"
+%                                   field will generate a random
+%                                   problem instance.
+%
+%
+%
+% - problem_description.data_test:  Data structure to compute distances for the "unknown" (to the algorithm) distances.
+%                                   It contains the 3 fields that are shown
+%                                   below. An empty "data_test" structure
+%                                   will do nothing.
+%
+%       -- data_test.entries:       A column vector consisting of "unknown" (to the algorithm)
+%                                   distances. An empty "data_test.entries"
+%                                   field will do nothing.
+%       -- data_test.rows:          The row position of th corresponding
+%                                   distances. An empty "data_test.rows"
+%                                   field will do nothing.
+%       -- data_test.cols:          The column position of th corresponding
+%                                   distances. An empty "data_test.cols"
+%                                   field will do nothing.
+%
+%
+%
+% - problem_description.n:          The number of data points. An empty
+%                                   "n", but complete "data_train" structure
+%                                   will lead to an error, to avoid
+%                                   potential data inconsistency.
+%
+%
+%
+% - problem_description.fixedrank_algo: Fixe-rank algorithm. Options are
+%                                       'TR' for trust-regions,
+%                                       'CG' for conjugate gradients,
+%                                       'SD' for steepest descent.
+%                                       Default, it is 'TR'.
+%
+%
+%
+%
+% - problem_description.rank_initial: Starting rank. Default, it is 1.
+%
+%
+%
+% - problem_description.rank_max:     Maximum rank. Default, it is equal to
+%                                     "problem_description.n".
+%
+%
+%
+%
+% - problem_description.params:  Structure array containing algorithm
+%                                parameters for stopping criteria.
+%       -- params.abstolcost:    Tolerance on absolute value of cost.
+%                                Default, it is 1e-3.
+%
+%
+%       -- params.reltolcost:    Tolerance on absolute value of cost.
+%                                Default, it is 1e-3.
+%       -- params.tolgradnorm:   Tolerance on the norm of the gradient.
+%                                Default, it is 1e-5.
+%       -- params.tolSmin:       Tolerance on smallest eigenvalue of Sy,
+%                                the dual variable.
+%                                Default, it is 1e-5.
+%       -- params.tolrankdeficiency:   Tolerance on the
+%                                      smallest singular value of Y.
+%                                      Default, it is 1e-3.
+%                           .
+%
+% Outputs:
+% --------
+%
+%   Y:                        n-by-r solution matrix of rank r.
+%   out_infos:                Structure array with additional information.
+%   out_problem_description:  Structure array with probelm description.
+%
+%
+%
+% Please cite the Manopt paper as well as the research paper:
+%     @InProceedings{mishra2014r3mc,
+%       Title        = {Low-rank optimization for distance matrix completion},
+%       Author       = {Mishra, B. and Meyer, G. and Sepulchre, R.},
+%       Booktitle    = {{50th IEEE Conference on Decision and Control}},
+%       Year         = {2011},
+%       Organization = {{IEEE CDC}}
+%     }
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Bamdev Mishra, April 06, 2015.
-% Contributors:
+% Contributors: Nicolas Boumal.
 % Change log:
-    clear all; clc; close all;
+
+
+    %% Check whether we have complete problem description. 
+    if ~exist('problem_desription', 'var')...
+            || ~isempty(problem_description)...
+            || ~all(isfield(problem_description,{'data_train'}) == 1)...
+            || ~all(isfield(problem_description.data_train,{'cols', 'rows', 'entries'}) == 1)...
+            || isempty(problem_description.data_train.cols)...
+            || isempty(problem_description.data_train.rows)...
+            || isempty(problem_description.data_train.entries)
+        
+        %% Generating a random instance
+        n = 500; % Number of points
+        r = 5; % Embedding dimension
+        
+        % Fraction of unknown distances
+        fractionOfUnknown = 0.8;
+        
+        
+        Yo = randn(n,r); % True embedding
+        trueDists = pdist(Yo)'.^2; % True distances
+        
+        % Add noise (set noise_level = 0 for clean measurements)
+        noise_level = 0.01;
+        trueDists = trueDists + noise_level * std(trueDists) * randn(size(trueDists));
+        
+        % Compute all pairs of indices
+        H = tril(true(n), -1);
+        [I, J] = ind2sub([n, n], find(H(:)));
+        clear 'H';
+        
+        % Train data
+        train = false(length(trueDists), 1);
+        train(1:floor(length(trueDists)*(1- fractionOfUnknown))) = true;
+        train = train(randperm(length(train)));
+        
+        data_train.rows = I(train);
+        data_train.cols = J(train);
+        data_train.entries = trueDists(train);
+        data_train.nentries = length(data_train.entries);
+        
+        % Test data
+        data_test.nentries = 1*data_train.nentries; % Depends how big data can we handle.
+        test = false(length(trueDists),1);
+        test(1 : floor(data_test.nentries)) = true;
+        test = test(randperm(length(test)));
+        data_test.rows = I(test);
+        data_test.cols = J(test);
+        data_test.entries = trueDists(test);
+        
+        % Fixed rank algorithm
+        fixedrank_algo = 'TR'; % Trust-regions.
+        
+        
+        % Rank bounds
+        rank_initial = 1; % Starting rank.
+        rank_max = n; % Maximum rank.
+        
+        % Basic parameters used in the scheme.
+        params.abstolcost = 1e-3;
+        params.reltolcost = 1e-3;
+        params.tolgradnorm = 1e-5;
+        params.tolSmin = -1e-3;
+        params.tolrankdeficiency = 1e-3;
+        
+        
+        
+        % Collect the problem description that are we solving.
+        out_problem_description.data_train = data_train;
+        out_problem_description.data_test = data_test;
+        out_problem_description.n = n;
+        out_problem_description.rank_initial = 1;
+        out_problem_description.rank_maximum = n;
+        out_problem_description.params = params;
+        out_problem_description.fixedrank_algo = 'TR';
+        
+    else
+        
+        %% Number of data points.
+        if isfield(problem_description, 'n')
+            n = problem_description.n;
+        else
+            error('problem_description:field_n_notfound','Error. Number of data points must be given \n');
+        end
+        out_problem_description.n = n;
+        
+        
+        %% Which fixed rank algorithm to use.
+        if isfield(problem_description, 'fixedrank_algo')
+            fixedrank_algo = problem_description.fixedrank_algo;
+        else
+            fixedrank_algo = 'TR';
+        end
+        out_problem_description.fixedrank_algo = fixedrank_algo;
+        
+        
+        
+        
+        %% Initial rank.
+        if isfield(problem_description, 'rank_initial') && ~isempty(problem_description.rank_initial)
+            rank_initial = problem_description.rank_initial;
+        else
+            rank_initial = 1;
+        end
+        out_problem_description.rank_initial = rank_initial;
+        
+        
+        %% Maximum rank that is allowed.
+        if isfield(problem_description, 'rank_max') && ~isempty(problem_description.rank_max)
+            rank_max = problem_description.rank_max;
+        else
+            rank_max = n;
+            out_problem_description.rank_maximum = rank_max;
+        end
+        
+        
+        %% Checking if there is a testing dataset.
+        if ~isfield(problem_description,{'data_test'})...
+                || ~all(isfield(problem_description.data_test,{'cols', 'rows', 'entries'}) == 1)...
+                || isempty(problem_description.data_test.cols)...
+                || isempty(problem_description.data_test.rows)...
+                || isempty(problem_description.data_test.entries)
+            data_test = [];
+        end
+        out_problem_description.data_test = data_test;
+        
+        
+        %% Parameters
+        if isfield(problem_description, 'params')
+            params = problem_description.params;
+            
+            if ~isfield(params,'abstolcost');  params.abstolcost = 1e-3; end
+            if ~isfield(params,'reltolcost');  params.reltolcost = 1e-3; end
+            if ~isfield(params,'tolgradnorm');  params.tolgradnorm = 1e-5; end
+            if ~isfield(params,'tolSmin');  params.tolSmin = -1e-3; end
+            if ~isfield(params,'tolrankdeficiency');  params.tolrankdeficiency = 1e-3; end
+            
+        else
+            % Basic parameters used in the scheme.
+            params.abstolcost = 1e-3;
+            params.reltolcost = 1e-3;
+            params.tolgradnorm = 1e-5;
+            params.tolSmin = -1e-3;
+            params.tolrankdeficiency = 1e-3;
+            
+        end
+        out_problem_description.params = params;
+        
+        
+    end
     
-    n = 500; % Number of points
-    r = 5; % Embedding dimension
     
-    % Fraction of unknown distances
-    fractionOfUnknown = 0.8;
-    
-    
-    Yo = randn(n,r); % True embedding
-    trueDists = pdist(Yo)'.^2; % True distances
-    
-    % Add noise (set noise_level = 0 for clean measurements)
-    noise_level = 0.01;
-    trueDists = trueDists + noise_level * std(trueDists) * randn(size(trueDists));
-    
-    % Compute all pairs of indices
-    H = tril(true(n), -1);
-    [I, J] = ind2sub([n, n], find(H(:)));
-    clear 'H';
-    
-    % Train data
-    train = false(length(trueDists), 1);
-    train(1:floor(length(trueDists)*(1- fractionOfUnknown))) = true;
-    train = train(randperm(length(train)));
-    
-    data_train.rows = I(train);
-    data_train.cols = J(train);
-    data_train.entries = trueDists(train);
-    data_train.nentries = length(data_train.entries);
-    
-    % Test data
-    data_test.nentries = 1*data_train.nentries; % Depends how big data can we handle.
-    test = false(length(trueDists),1);
-    test(1 : floor(data_test.nentries)) = true;
-    test = test(randperm(length(test)));
-    data_test.rows = I(test);
-    data_test.cols = J(test);
-    data_test.entries = trueDists(test);
-    
-   
     %% Rank search algorithm
-    r_initial = 1;
-    rmax = n;
     N = data_train.nentries; % Number of known distances
-    
     EIJ = speye(n);
     EIJ = EIJ(:, data_train.rows) - EIJ(:, data_train.cols);
     
-    rr = r_initial;
+    rr = rank_initial;
     Y = randn(n, rr);
     
     time = [];
     cost = [];
     test_error = [];
-    % Optimization main loop
-    while (rr <= rmax), % When r = n a global min is attained for sure.
+    rank = [];
+    
+    
+    while (rr <= rank_max), % When r = n a global min is attained for sure.
         fprintf('>> Rank %d <<\n', rr);
-        if (rr > r_initial),
+        if (rr > rank_initial),
             if isempty(restartDir), % If no restart dir avail. do random restart
                 
                 disp('No restart dir available, random restart is performed');
@@ -118,8 +343,8 @@ function lowrank_dist_completion( )
             
         end
         
-        % Run fixed-rank optimization using Manopt
-        [Y, infos] = lowrank_dist_completion_fixedrank(data_train, data_test, Y);
+        % Run the fixed-rank optimization using Manopt.
+        [Y, infos] = lowrank_dist_completion_fixedrank(fixedrank_algo, data_train, data_test, Y, params);
         
         % Some info logging.
         thistime = [infos.time];
@@ -128,11 +353,13 @@ function lowrank_dist_completion( )
         end
         time = [time thistime]; %#ok<AGROW>
         cost = [cost [infos.cost]]; %#ok<AGROW>
+        rank = [rank [infos.rank]];
+        
         if isfield(infos, 'test_error')
             test_error = [test_error [infos.test_error]]; %#ok<AGROW>
         end
         
-        % Evaluate gradient of the convex cost function (i.e. wrt X)
+        % Evaluate gradient of the convex cost function (i.e. wrt X).
         Z = Y(data_train.rows, :) - Y(data_train.cols,:);
         estimDists = sum(Z.^2,2);
         errors = (estimDists - data_train.entries);
@@ -145,7 +372,7 @@ function lowrank_dist_completion( )
         % as well as a way to control progress toward the global
         % optimum (s_min).
         
-        % Make eigs silent
+        % Make eigs silent.
         opts.disp = 0;
         opts.issym = true;
         [v, s_min] = eigs(Sy, 1, 'SA', opts);
@@ -155,7 +382,7 @@ function lowrank_dist_completion( )
         
         % Stopping criterion.
         fprintf('>> smin = %.3e, and min(vp) = %.3e\n',s_min,min(vp));
-        if (s_min  > -1e-3) || (min(vp) < 1e-3),
+        if (s_min  > params.tolSmin) || (min(vp) < params.tolrankdeficiency),
             break;
         end
         
@@ -170,6 +397,15 @@ function lowrank_dist_completion( )
         
     end
     
+    
+    %% Collect the basic informationa that we have.
+    out_infos.time = time;
+    out_infos.cost = cost;
+    out_infos.rank = rank;
+    out_infos.test_error = test_error;
+    
+    
+    %% Some plots
     fs = 20;
     figure;
     semilogy(cost, '-O', 'Color', 'blue', 'linewidth', 2.0);
@@ -189,7 +425,7 @@ function lowrank_dist_completion( )
         ax1 = gca;
         set(ax1, 'FontSize', fs);
         xlabel(ax1, 'Iterations', 'FontSize', fs);
-        ylabel(ax1, 'Test eror', 'FontSize', fs);
+        ylabel(ax1, 'Test error', 'FontSize', fs);
         legend('Trust-regions');
         legend 'boxoff';
         box off;
@@ -201,17 +437,17 @@ end
 
 
 
-function [Yopt, infos] = lowrank_dist_completion_fixedrank(data_train, data_test, Y_initial)
+function [Yopt, infos] = lowrank_dist_completion_fixedrank(fixedrank_algo, data_train, data_test, Y_initial, params)
     
+    %% Common
     [n, r] = size(Y_initial);
     EIJ = speye(n);
     EIJ = EIJ(:, data_train.rows) - EIJ(:, data_train.cols);
     
-    % Create the problem structure
-    % quotient YYt (tuned for least square problems) geometry
+    %% Create the problem structure
     problem.M = symfixedrankYYfactory(n,  r);
     
-    
+    %% Cost evaluation
     problem.cost = @cost;
     function [f, store] = cost(Y, store)
         if ~isfield(store, 'xij')
@@ -222,6 +458,7 @@ function [Yopt, infos] = lowrank_dist_completion_fixedrank(data_train, data_test
         f = 0.5*mean((estimDists - data_train.entries).^2);
     end
     
+    %% Gradient evaluation.
     problem.grad = @grad;
     function [g, store] = grad(Y, store)
         N = data_train.nentries;
@@ -233,6 +470,7 @@ function [Yopt, infos] = lowrank_dist_completion_fixedrank(data_train, data_test
         g = EIJ * sparse(1:N,1:N,2 * (estimDists - data_train.entries) / N, N, N) * xij;
     end
     
+    %% Hessian evaluation.
     problem.hess = @hess;
     function [Hess, store] = hess(Y, eta, store)
         N = data_train.nentries;
@@ -258,7 +496,7 @@ function [Yopt, infos] = lowrank_dist_completion_fixedrank(data_train, data_test
     
     
     
-    % Ask Manopt to compute the test error at every iteration
+    %% When asked, ask Manopt to compute the test error at every iteration.
     if ~isempty(data_test)
         options.statsfun = @compute_test_error;
         EIJ_test = speye(n);
@@ -268,20 +506,27 @@ function [Yopt, infos] = lowrank_dist_completion_fixedrank(data_train, data_test
         xij = EIJ_test'*Y;
         estimDists_test = sum(xij.^2,2);
         stats.test_error = 0.5*mean((estimDists_test - data_test.entries).^2);
+        stats.rank = r;
     end
-
+    
+    %% Stopping criteri
     
     options.stopfun = @mystopfun;
     function stopnow = mystopfun(problem, Y, info, last) %#ok<INUSL>
-        stopnow = (last >= 3 && (info(last-2).cost - info(last).cost < 1e-3 || abs(info(last-2).cost - info(last).cost)/info(last).cost < 1e-3));
+        stopnow = (last >= 3 && (info(last-2).cost - info(last).cost < params.abstolcost || abs(info(last-2).cost - info(last).cost)/info(last).cost < params.reltolcost));
     end
-    options.tolgradnorm = 1e-5;
+    options.tolgradnorm = params.tolgradnorm;
     
-    
-    
-    [Yopt, ~, infos] = trustregions(problem, Y_initial, options);
-    
-    
+    %% Call the appropriate algorithm
+    if strcmp(fixedrank_algo, 'TR'),
+        [Yopt, ~, infos] = trustregions(problem, Y_initial, options);
+    elseif strcmp(fixedrank_algo, 'CG'),
+        [Yopt, ~, infos] = conjugategradient(problem, Y_initial, options);
+    elseif strcmp(fixedrank_algo, 'SD'),
+        [Yopt, ~, infos] = steepestdescent(problem, Y_initial, options);
+    else % Default
+        [Yopt, ~, infos] = trustregions(problem, Y_initial, options);
+    end
     
 end
 
