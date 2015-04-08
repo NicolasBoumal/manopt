867245e23b5bbc6498699f9e83f2d87dd0530d48
Nicolas Boumal <nicolasboumal@gmail.com>
2015-04-08 10:47:06 +0200

    robust_pca now has auto data gen so you cna just run it

--- examples/robust_pca.m
+++ examples/robust_pca.m
@@ -1,30 +1,35 @@
-function [U, cost] = robustpca(X, d)
-% Compute a robust version of PCA (principal component analysis) on data.
+function [U, cost] = robust_pca(X, d)
+% Computes a robust version of PCA (principal component analysis) on data.
 % 
 % function [U, cost] = robustpca(X, d)
 %
 % Given a matrix X of size p by n, such that each column represents a
 % point in R^p, this computes U: an orthonormal basis of size p by d such
 % that the column space of U captures the points X as well as possible.
-% More precisely, the function attempts to compute U as the minimizer,
+% More precisely, the function attempts to compute U as the minimizer
 % over the Grassmann manifold (the set of linear subspaces) of:
 %
 %  f(U) = (1/n) Sum_{i = 1:n} dist(X(:, i), the space spanned by U)
 %       = (1/n) Sum_{i = 1:n} || U*U'*X(:, i) - X(:, i) ||
 %
-% The output cost represents the sum of distances divided by n achieved
-% with the returned U. Notice that norms are not squared, for robustness.
+% The output cost represents the average distance achieved with the
+% returned U. Notice that norms are not squared, for robustness.
 %
-% In practice, because this function is nonsmooth, we smooth it with a
+% In practice, because this function is nonsmooth, it is smoothed with a
 % pseudo-Huber loss function of parameter epsilon (noted e for short), and
-% we iteratively reduce the smoothing parameter:
+% the smoothing parameter is iteratively reduced (with warm starts):
 %
 %   f_e(U) = (1/n) Sum_{i = 1:n} l_e(|| U*U'*X(:, i) - X(:, i) ||)
 %
 %   with l_e(x) = sqrt(x^2 + e^2) - e (for e = 0, this is absolute value).
 %
 % The intermediate optimization of the smooth cost over the Grassmann
-% manifold are performed using the Manopt toolbox.
+% manifold is performed using the Manopt toolbox.
+%
+% Ideally, the non-outlier data should be centered. If not, this
+% pre-processing centers all the data, but bear in mind that outliers will
+% shift the center of mass too.
+% X = X - repmat(mean(X, 2), [1, size(X, 2)]);
 %
 % There are no guarantees that this code will return the optimal U.
 % This code is distributed to illustrate one possible way of optimizing
@@ -33,33 +38,6 @@ function [U, cost] = robustpca(X, d)
 % global optimizer with high probability, under some probabilistic model
 % on the data and some conditions on the noise.
 % For practical use, the constants in the code would need to be tuned.
-%
-%
-% Here is code for a quick test with data in the plane and fitting a line:
-% clear all;
-% close all;
-% clc;
-% % Generate some data points aligned on a subspace
-% X = rand(2,1)*(1:30) + .02*randn(2, 30).*[(1:30);(1:30)];
-% P = randperm(size(X,2));
-% % And add some random outliers to the mix
-% outliers = 10;
-% X(:, P(1:outliers)) = 30*randn(2, outliers);
-% % Center the data
-% X = X - repmat(mean(X, 2), [1, size(X, 2)]);
-% % Compute a robust PCA on the data and plot the results
-% U = robustpca(X, 1);
-% scatter(X(1,:), X(2,:));
-% hold on;
-% plot(U(1)*[-1, 1]*100, U(2)*[-1 1]*100, 'r');
-% hold off;
-% % Compare to a standard PCA
-% [Upca,~,~] = svds(X,1);
-% hold on;
-% plot(Upca(1)*[-1, 1]*100, Upca(2)*[-1 1]*100, 'k');
-% hold off;
-% xlim(1.1*[min(X(1,:)), max(X(1,:))]);
-% ylim(1.1*[min(X(2,:)), max(X(2,:))]);
 
 % This file is part of Manopt and is copyrighted. See the license file.
 %
@@ -67,10 +45,35 @@ function [U, cost] = robustpca(X, d)
 % Contributors:
 %
 % Change log:
+%
 %   March 4, 2015 (NB):
 %       Uses a pseudo-Huber loss rather than a Huber loss: this has the
 %       nice advantage of being smooth and simpler to code (no if's).
+%
+%   April 8, 2015 (NB):
+%       Built-in test data for quick tests; added comment about centering.
 
+
+
+    % If no inputs, generate random data for illustration purposes.
+    if nargin == 0
+        % Generate some data points aligned on a subspace
+        X = rand(2, 1)*(1:30) + .05*randn(2, 30).*[(1:30);(1:30)];
+        % And add some random outliers to the mix
+        P = randperm(size(X, 2));
+        outliers = 10;
+        X(:, P(1:outliers)) = 30*randn(2, outliers);
+        % Center the data
+        % X = X - repmat(mean(X, 2), [1, size(X, 2)]);
+        d = 1;
+    end
+
+
+
+
+
+    % Prepare a Manopt problem structure for optimization of the given
+    % cost (defined below) over the Grassmann manifold.
     [p, n] = size(X);
     manifold = grassmannfactory(p, d);
     problem.M = manifold;
@@ -81,24 +84,46 @@ function [U, cost] = robustpca(X, d)
 	% This is just one idea: it is not necessarily useful or ideal.
     [U, ~, ~] = svds(X, d);
 
+    
 	% Iteratively reduce the smoothing constant epsilon and optimize
 	% the cost function over Grassmann.
     epsilon = 1;
-    
 	n_iterations = 6;
 	reduction = .5;
 	options.verbosity = 2; % Change this number for more or less output
     warning('off', 'manopt:getHessian:approx');
     for iter = 1 : n_iterations
-        U = trustregions(problem, U, options);
+        U = trustregions(problem, [], options);
         epsilon = epsilon * reduction;
     end
     warning('on', 'manopt:getHessian:approx');
     
+    
 	% Return the cost as the actual sum of distances, not smoothed.
 	epsilon = 0;
 	cost = robustpca_cost(U);
+    
+    
+    
+    % If working with the auto-generated input, plot the results.
+    if nargin == 0
+        scatter(X(1,:), X(2,:));
+        hold on;
+        plot(U(1)*[-1, 1]*100, U(2)*[-1 1]*100, 'r');
+        hold off;
+        % Compare to a standard PCA
+        [Upca, ~, ~] = svds(X,1);
+        hold on;
+        plot(Upca(1)*[-1, 1]*100, Upca(2)*[-1 1]*100, 'k');
+        hold off;
+        xlim(1.1*[min(X(1,:)), max(X(1,:))]);
+        ylim(1.1*[min(X(2,:)), max(X(2,:))]);
+        legend('data points', 'Robust PCA fit', 'Standard PCA fit');
+    end
 
+    
+    
+    % Smoothed cost
     function value = robustpca_cost(U)
 
         vecs = U*(U'*X) - X;
@@ -108,6 +133,8 @@ function [U, cost] = robustpca(X, d)
 
     end
 
+    % Euclidean gradient of the smoothed cost (it will be transformed into
+    % the Riemannian gradient automatically by Manopt).
     function G = robustpca_gradient(U)
 
 		% Note that the computation of vecs and sqnrms is redundant
@@ -120,7 +147,7 @@ function [U, cost] = robustpca(X, d)
         sqnrms = sum(vecs.^2, 1);
         % This explicit loop is a bit slow: the code below is equivalent
         % and faster to compute the gradient.
-        % G=zeros(p, d);
+        % G = zeros(p, d);
         % for i=1:n
         %     G = G + (1/sqrt(sqnrms(i) + epsilon^2)) * vecs(:,i) * UtX(:,i)';
         % end
