0bbd2d3cdb3a970c1fd175f3c8afcdf719f68f36
Nicolas Boumal <nicolasboumal@gmail.com>
2015-06-17 20:05:25 +0200

    cosmetic

--- manopt/manifolds/fixedranktensors/tucker2multiarray.m
+++ manopt/manifolds/fixedranktensors/tucker2multiarray.m
@@ -1,13 +1,15 @@
-function GU1U2U3 =  tucker2multiarray(X)
-% Converts a 3d Tucker form to an multiarray
+function GU1U2U3 = tucker2multiarray(X)
+% Converts a 3d Tucker form to a multiarray
 %
-% function Xtensor =  tucker2multiarray(X)
+% function Xtensor = tucker2multiarray(X)
 %
 % X has fields U1, U2, U3, and G.
 %
-% The matrices U1 (n1-by-r1), U2 (n2-by-r2) and U3 (n3-by-r3) are orthogonal
-% matrices.
+% The matrices U1 (n1-by-r1), U2 (n2-by-r2) and U3 (n3-by-r3) are
+% orthogonal matrices.
 % G (r1-by-r2-by-r3) is a multidimensional array.
+%
+% See also fixedrankfactory_tucker_preconditioned
 
 % This file is part of Manopt: www.manopt.org.
 % Original authors: Hiroyuki Kasai and Bamdev Mishra, June 05, 2015.
@@ -19,13 +21,13 @@ function GU1U2U3 =  tucker2multiarray(X)
     U3 = X.U3;
     G = X.G;
     
-    % Tesnor size
+    % Tensor size
     n1 = size(U1, 1);
     n2 = size(U2, 1);
     n3 = size(U3, 1);
     
     % Core size
-    [r1 r2 r3] = size(G);
+    [r1, r2, r3] = size(G);
     
     % Multplication by U1
     G1 = reshape(G, r1, r2*r3);
@@ -40,4 +42,5 @@ function GU1U2U3 =  tucker2multiarray(X)
     
     
     GU1U2U3 = permute(reshape(U3*G3, n3, n1, n2), [2 3 1]);
+    
 end
