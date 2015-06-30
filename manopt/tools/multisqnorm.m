c6cd1cd484ebdf521f3145c509ddc2edac8b7510
Nicolas Boumal <nicolasboumal@gmail.com>
2015-06-30 21:53:41 +0200

    cosmetic

--- manopt/tools/multisqnorm.m
+++ manopt/tools/multisqnorm.m
@@ -6,7 +6,7 @@ function sqnorm = multisqnorm(A)
 % Given a 3-dimensional matrix A of size n-by-m-by-N, returns a column
 % vector of length N such that sqnorm(i) = norm(A(:, :, i), 'fro')^2.
 %
-% See also: multiprod multitransp multitrace
+% See also: multiprod multitransp multitrace norms
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, June 17, 2015.
@@ -19,6 +19,7 @@ function sqnorm = multisqnorm(A)
             'or less dimensions.']);
 	[n, m, N] = size(A);
     
+    % This is equivalent to squeeze(sum(norms(A, 2, 1).^2)), but faster.
     sqnorm = sum(reshape(A, n*m, N).^2, 1)';
 
 end
