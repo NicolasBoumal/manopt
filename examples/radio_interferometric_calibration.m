ab8098fed5999ba2b78c3b17af2da88579c2f5e0
Nicolas Boumal <nicolasboumal@gmail.com>
2016-06-28 11:49:31 -0400

    Cosmetic in comment

--- examples/radio_interferometric_calibration.m
+++ examples/radio_interferometric_calibration.m
@@ -38,8 +38,8 @@ function xsol = radio_interferometric_calibration(N, K)
 %   June 28, 2016 (BM):
 %       Modified the egrad and ehess operations according to 
 %       the modified metric in the symfixedrankYYcomplexfactory file, 
-%       where a factor of 2 was removede from the metric. Subsequently, 
-%       a factor of 2 was multiplied to egrad and ehess operations.
+%       where a factor of 2 was removed from the metric. Accordingly, 
+%       a factor of 2 was added to egrad and ehess operations.
     
     % Generate some random data to test the function
     
