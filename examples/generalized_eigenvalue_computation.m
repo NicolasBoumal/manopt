5a54b3687902ad9a7eea3de98f007a9f2dcc02ed
Nicolas Boumal <nicolasboumal@gmail.com>
2016-08-10 15:23:31 -0400

    Added a rotation of Xsol at the end of the example file to match the definition of generalized eigenvectors (the eigenvalues were fine.) Follows a message on the forum.

--- examples/generalized_eigenvalue_computation.m
+++ examples/generalized_eigenvalue_computation.m
@@ -4,31 +4,36 @@ function [Xsol, Ssol] = generalized_eigenvalue_computation(A, B, p)
 % function [Xsol, Ssol] = generalized_eigenvalue_computation(A, B, p)
 %
 % Input: A is a real, symmetric matrix of size nxn,
-%        B is a symmetric positive definite matrix, same size as A, and
-%        an integer p < n.
-% Output: Xsol: a real, orthonormal matrix X of size nxp such that
-%         trace(X'*A*X) is maximized, subject to X'*B*X being identity. 
-%         That is, the columns of X form an orthonormal basis of a dominant
-%         subspace of dimension p of B^(-1)*A. These are thus eigenvectors
-%         associated with the largest eigenvalues of B^(-1)*A  (in no
-%         particular order). Sign is important: 2 is deemed a larger
-%         eigenvalue than -5.
-%         Ssol: the eigenvalues associated with the eigenvectors Xsol.
+%        B is a symmetric positive definite matrix, same size as A
+%        p is an integer such that p <= n.
+%
+% Output: Xsol: a real, B-orthonormal matrix X of size nxp such that
+%         trace(X'*A*X) is maximized, subject to X'*B*X = identity. 
+%         That is, the columns of X form a B-orthonormal basis of a
+%         dominant subspace of dimension p of B^(-1)*A. These are thus
+%         generalized eigenvectors associated with the largest generalized
+%         eigenvalues of B^(-1)*A  (in no particular order). Sign is
+%         important: 2 is deemed a larger eigenvalue than -5.
+%         Ssol: the eigenvalues associated with the eigenvectors Xsol, in a
+%         vector.
 % 
 % We intend to solve the homogeneous system A*X = B*X*S,
 % where S is a diagonal matrix of dominant eigenvalues of B^-1 A.
 %
 %
 % The optimization is performed on the generalized Grassmann manifold, 
-% since only the space spanned by the columns of X matters. 
+% since only the space spanned by the columns of X matters in the
+% optimization problem.
 %
 % The optimization problem that we are solving here is 
 % maximize trace(X'*A*X) subject to X'*B*X = eye(p). 
 % Consequently, the solutions remain invariant to transformation
-% X --> XO, where O is a p-by-p orthogonal matrix. The search space, in
+% X --> XQ, where Q is a p-by-p orthogonal matrix. The search space, in
 % essence, is set of equivalence classes
-% [X] = {XO : X'*B*X = I and O is orthogonal matrix}. This space is called
+% [X] = {XQ : X'*B*X = I and Q is orthogonal matrix}. This space is called
 % the generalized Grassmann manifold.
+% Before returning, Q is chosen such that Xsol = Xq matches the output one
+% would expect from eigs.
 %
 % See also dominant_invariant_subspace nonlinear_eigenspace
 
@@ -38,6 +43,10 @@ function [Xsol, Ssol] = generalized_eigenvalue_computation(A, B, p)
 % Main author: Bamdev Mishra, June 30, 2015.
 % Contributors:
 % Change log:
+%
+%     Aug. 10, 2016 (NB): the eigenvectors Xsol are now rotated by Vsol
+%     before they are returned, to ensure the output matches what you would
+%     normally expect calling eigs.
     
     % Generate some random data to test the function
     if ~exist('A', 'var') || isempty(A)
@@ -82,10 +91,20 @@ function [Xsol, Ssol] = generalized_eigenvalue_computation(A, B, p)
     % Issue a call to a solver. A random initial guess will be chosen and
     % default options are selected except for the ones we specify here.
     options.Delta_bar = 8*sqrt(p);
-    [Xsol, costXsol, info] = trustregions(problem, [], options); %#ok<NASGU,ASGLU>
+    options.tolgradnorm = 1e-7;
+    options.verbosity = 2; % set to 0 to silence the solver, 2 for normal output.
+    [Xsol, costXsol, info] = trustregions(problem, [], options); %#ok<ASGLU>
     
     % To extract the eigenvalues, solve the small p-by-p symmetric 
     % eigenvalue problem.
-    Ssol = eig(Xsol'*(A*Xsol));
+    [Vsol, Dsol] = eig(Xsol'*(A*Xsol));
+    Ssol = diag(Dsol);
+    
+    % To extract the eigenvectors, rotate Xsol by the p-by-p orthogonal
+    % matrix Vsol.
+    Xsol = Xsol*Vsol;
+    
+    % This quantity should be small.
+    % norm(A*Xsol - B*Xsol*diag(Ssol));
   
 end
