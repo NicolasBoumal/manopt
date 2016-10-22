a2fe7dd21e66034ac00f6f3a07b73497ce272e53
Nicolas Boumal <nicolasboumal@gmail.com>
2016-10-21 22:49:52 -0400

    Improvements in comments; corrected harmless bug in Erdos Renyi generation

--- examples/elliptope_SDP.m
+++ examples/elliptope_SDP.m
@@ -1,9 +1,9 @@
 function [Y, problem, S] = elliptope_SDP(A, p, Y0)
 % Solver for semidefinite programs (SDP's) with unit diagonal constraints.
 % 
-% function [Y, problem, S] = maxcut_simple(A)
-% function [Y, problem, S] = maxcut_simple(A, p)
-% function [Y, problem, S] = maxcut_simple(A, p, Y0)
+% function [Y, problem, S] = elliptope_SDP(A)
+% function [Y, problem, S] = elliptope_SDP(A, p)
+% function [Y, problem, S] = elliptope_SDP(A, p, Y0)
 %
 % A is a real, symmetric matrix of size n.
 %
@@ -23,28 +23,34 @@ function [Y, problem, S] = elliptope_SDP(A, p, Y0)
 % why Manopt is appropriate software to solve this problem. An optional
 % initial guess can be specified via the input Y0.
 %
-% See the following paper for theory, specifically for a proof that, for
+% See the following paper for theory, specifically, for a proof that, for
 % almost all A, second-order critical points of the problem in Y are
 % globally optimal. In other words: there are no local traps in Y, despite
 % non-convexity.
 %
+% Outputs:
+%
 %       Y: is the best point found (an nxp matrix with unit norm rows.)
+%          To find X, form Y*Y' (or, more efficiently, study X through Y.)
 % 
-% problem: is the Manopt problem structure used to produce Y.
+%       problem: is the Manopt problem structure used to produce Y.
 % 
 %       S: is a dual optimality certificate (a symmetric matrix of size n,
 %          sparse if A is sparse). The optimality gap (in the cost
 %          function) is at most n*min(eig(S)), for both Y and X = Y*Y'.
+%          Hence, if min(eig(S)) is close to zero, Y is close to globally
+%          optimal. This can be computed via eigs(S, 1, 'SR').
 % 
-% 
-% @article{boumal2016bmapproach,
+% Paper: https://arxiv.org/abs/1606.04970
+%
+% @inproceedings{boumal2016bmapproach,
 %   author  = {Boumal, N. and Voroninski, V. and Bandeira, A.S.},
 %   title   = {The non-convex {B}urer-{M}onteiro approach works on smooth semidefinite programs},
-%   journal = {arXiv preprint arXiv:1606.04970},
+%   booktitle={Neural Information Processing Systems (NIPS 2016)},
 %   year    = {2016}
 % }
 % 
-% See also: maxcut
+% See also: maxcut elliptope_SDP_complex
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, June 28, 2016
@@ -53,17 +59,20 @@ function [Y, problem, S] = elliptope_SDP(A, p, Y0)
 
 
     % If no inputs are provided, since this is an example file, generate
-    % an random Erdos-Renyi graph. This is for illustration purposes only.
+    % a random Erdos-Renyi graph. This is for illustration purposes only.
     if ~exist('A', 'var') || isempty(A)
         n = 100;
-        A = triu(randn(n) <= .1, 1);
-        A = A+A';
+        A = triu(rand(n) <= .1, 1);
+        A = (A+A.')/(2*n);
     end
 
     n = size(A, 1);
     assert(n >= 2, 'A must be at least 2x2.');
+    assert(isreal(A), 'A must be real.');
     assert(size(A, 2) == n, 'A must be square.');
-    assert(nnz(A-A.') == 0, 'A must be real, symmetric.');
+    
+    % Force A to be symmetric
+    A = (A+A.')/2;
     
     % By default, pick a sufficiently large p (number of columns of Y).
     if ~exist('p', 'var') || isempty(p)
@@ -72,14 +81,24 @@ function [Y, problem, S] = elliptope_SDP(A, p, Y0)
     
     assert(p >= 2 && p == round(p), 'p must be an integer >= 2.');
 
-    % Pick the manifold of nxp matrices with unit norm rows.
+    % Pick the manifold of n-by-p matrices with unit norm rows.
     manifold = obliquefactory(p, n, true);
     
     problem.M = manifold;
     
-
+    
+    % These three, quick commented lines of code are sufficient to define
+    % the cost function and its derivatives. This is good code to write
+    % when prototyping. Below, a more advanced use of Manopt is shown,
+    % where the redundant computation A*Y is avoided between the gradient
+    % and the cost evaluation.
+    % % problem.cost  = @(Y) .5*sum(sum((A*Y).*Y));
+    % % problem.egrad = @(Y) A*Y;
+    % % problem.ehess = @(Y, Ydot) A*Ydot;
+    
     % Products with A dominate the cost, hence we store the result.
     % This allows to share the results among cost, grad and hess.
+    % This is completely optional.
     function store = prepare(Y, store)
         if ~isfield(store, 'AY')
             AY = A*Y;
@@ -110,6 +129,7 @@ function [Y, problem, S] = elliptope_SDP(A, p, Y0)
         H = manifold.proj(Y, SYdot);
     end
 
+
     % If no initial guess is available, tell Manopt to use a random one.
     if ~exist('Y0', 'var') || isempty(Y0)
         Y0 = [];
@@ -117,8 +137,9 @@ function [Y, problem, S] = elliptope_SDP(A, p, Y0)
 
     % Call your favorite solver.
     opts = struct();
-    opts.verbosity = 0;     % Set to 0 for no output, 2 for normal output
-    opts.maxinner = 500;
+    opts.verbosity = 0;      % Set to 0 for no output, 2 for normal output
+    opts.maxinner = 500;     % maximum Hessian calls per iteration
+    opts.tolgradnorm = 1e-6; % tolerance on gradient norm
     Y = trustregions(problem, Y0, opts);
     
     % If required, produce an optimality certificate.
