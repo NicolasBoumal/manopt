f6cc902537b8385e57aee0b8dbef05a0287305e1
Nicolas Boumal <nicolasboumal@gmail.com>
2016-11-12 12:56:22 -0500

    Turned thomson_problem into a function with proper comments

--- examples/thomson_problem.m
+++ examples/thomson_problem.m
@@ -1,26 +1,41 @@
-% This script is an example of how Manopt can approximate the gradient and
-% even the Hessian of a cost function based on finite differences, if only
+function X = thomson_problem(n, d)
+% Simple attempt at computing n well distributed points on a sphere in R^d.
+% 
+% This is an example of how Manopt can approximate the gradient and even
+% the Hessian of a cost function based on finite differences, even if only
 % the cost function is specified without its derivatives.
 %
 % This functionality is provided only as a help for prototyping, and should
 % not be used to compare algorithms in terms of computation time or
-% accuracy.
+% accuracy, because the underlying gradient approximation scheme is slow.
 %
 % See also the derivative free solvers for an alternative:
 % pso and neldermead.
 
+% This file is part of Manopt: www.manopt.org.
+% Original author: Nicolas Boumal, Nov. 1, 2016
+% Contributors:
+% Change log:
+
+if ~exist('n', 'var') || isempty(n)
+    n = 50;
+end
+if ~exist('d', 'var') || isempty(d)
+    d = 3;
+end
+
 % Define the Thomson problem with 1/r^2 potential. That is: find n points
-% x_i on a sphere in R^3 such that the sum over all pairs (i, j) of the
+% x_i on a sphere in R^d such that the sum over all pairs (i, j) of the
 % potentials 1/||x_i - x_j||^2 is minimized. Since the points are on a
 % sphere, each potential is .5/(1-x_i'*x_j).
-n = 50;
-problem.M = obliquefactory(3, n);
+problem.M = obliquefactory(d, n);
 problem.cost = @(X) sum(sum(triu(1./(1-X'*X), 1))) / n^2;
 
-% Attempt to minimize the cost. Since the gradient is not provided, it is
-% approximated with finite differences. This is /slow/, since for each
+% Attempt to minimize the cost. Since the gradient is not provided, Manopt
+% approximates it with finite differences. This is /slow/, since for each
 % gradient approximation, problem.M.dim()+1 calls to the cost function are
-% necessary.
+% necessary, on top of generating an orthonormal basis of the tangent space
+% at each iterate.
 %
 % Note that it is difficult to reach high accuracy critical points with an
 % approximate gradient, hence the need to set a less ambitious value for
@@ -29,11 +44,15 @@ opts.tolgradnorm = 1e-3;
 X = conjugategradient(problem, [], opts);
 
 % Plot the points on a translucid sphere
-[x, y, z] = sphere(50);
-surf(x, y, z, 'FaceAlpha', .5);
-hold all;
-plot3(X(1, :), X(2, :), X(3, :), '.', 'MarkerSize', 20);
-axis equal;
+if nargout == 0 && d == 3
+    [x, y, z] = sphere(50);
+    surf(x, y, z, 'FaceAlpha', .5);
+    hold all;
+    plot3(X(1, :), X(2, :), X(3, :), '.', 'MarkerSize', 20);
+    axis equal;
+    box off;
+    axis off;
+end
 
 % For much better performance, after an early prototyping phase, the
 % gradient of the cost function should be specified, typically in
@@ -42,3 +61,5 @@ axis equal;
 %   http://www.manopt.org
 %
 % for more information.
+
+end
