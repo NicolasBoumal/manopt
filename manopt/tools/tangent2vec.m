e689bd6c17ce3e39806f3a1576d0bad5a4da217a
Nicolas Boumal <nicolasboumal@gmail.com>
2017-02-03 19:12:11 -0500

    Improved documentation relating the two.

--- manopt/tools/tangent2vec.m
+++ manopt/tools/tangent2vec.m
@@ -16,7 +16,14 @@ function vec = tangent2vec(M, x, basis, u)
 %
 %    u = sum_{k=1}^n  vec(k)*basis{k}    <- u = lincomb(M, x, basis, vec)
 %
-% See also: tangentorthobasis lincomb orthogonalize grammatrix
+% Note that tangent2vec is an isometry, that is, up to numerical round-off
+% errors, with u and v two tangent vectors at x:
+%
+%    M.inner(x, u, v)  ==  uu'*vv,
+%
+% where uu = tangent2vec(M, x, basis, u), vv = tangent2vec(M, x, basis, v).
+%
+% See also: tangentorthobasis lincomb orthogonalize grammatrix hessianmatrix
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, Feb. 3, 2017.
