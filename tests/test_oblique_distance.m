baf3861405322cb885c2ae06f4211ce138225094
Nicolas Boumal <nicolasboumal@gmail.com>
2017-07-20 16:42:44 +0200

    Same improvement to distance and logarithm for phase manifolds

--- tests/test_oblique_distance.m
+++ tests/test_oblique_distance.m
@@ -1,12 +1,14 @@
 % M = obliquecomplexfactory(5, 10, false);
 % M = obliquefactory(5, 10, false);
 % M = spherecomplexfactory(5, 10);
-M = spheresymmetricfactory(5);
+% M = spheresymmetricfactory(5);
+% M = complexcirclefactory(5);
+M = realphasefactory(5);
 
 X = M.rand();
 U = M.randvec(X);
 U = U / M.norm(X, U);
-t = 1e-10;
+t = 1e-9;
 Y = M.exp(X, U, t);
 
 abs(M.dist(X, Y) - t)/t
