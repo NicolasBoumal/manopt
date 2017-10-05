2d08a1810ad22b99809fb4e8864c14b20b7dc3d1
Nicolas Boumal <Nicolas Boumal>
2017-10-05 17:44:33 -0400

    Major changes to orthogonalize function (it's not a Modified Gram Schmidt) and added orthogonalizetwice for extra accuracy -- also include a test file, and kept the former algorithm to include in the tests

--- manopt/tools/orthogonalize.m
+++ manopt/tools/orthogonalize.m
@@ -1,7 +1,7 @@
-function [orthobasis, L] = orthogonalize(M, x, basis)
+function [Q, R] = orthogonalize(M, x, A)
 % Orthonormalizes a basis of tangent vectors in the Manopt framework.
 %
-% function [orthobasis, L] = orthogonalize(M, x, basis)
+% function [orthobasis, R] = orthogonalize(M, x, basis)
 %
 % M is a Manopt manifold structure obtained from a factory.
 % x is a point on the manifold M.
@@ -10,51 +10,53 @@ function [orthobasis, L] = orthogonalize(M, x, basis)
 % orthobasis is a cell of same size as basis which contains an orthonormal
 % basis for the same subspace as that spanned by basis. Orthonormality is
 % assessed with respect to the metric on the tangent space to M at x.
-% L is upper triangular of size n x n if basis has n vectors, such that,
-% basis{k} = sum_j=1^k orthobasis{j} * L(j, k) (akin to R in a QR
-% factorization.)
+% R is upper triangular of size n x n if basis has n vectors, such that:
 %
-% See also: grammatrix tangentorthobasis
+%   basis{k} = sum_j=1^k orthobasis{j} * R(j, k).
+%
+% That is: we compute a QR factorization of basis.
+%
+% The algorithm is a modified Gram-Schmidt. If elements in the input basis
+% are close to being linearly dependent (ill conditioned), then consider
+% orthogonalizing twice, or calling orthogonalizetwice directly.
+%
+% See also: orthogonalizetwice grammatrix tangentorthobasis
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, April 28, 2016.
 % Contributors: 
 % Change log: 
+%
+%       Oct. 5, 2017 (NB):
+%           Changed algorithm to a modified Gram-Schmidt and commented
+%           about the twice-is-enough trick. Compared to the previous
+%           version, this algorithm behaves much better if the input basis
+%           is ill conditioned.
 
+    assert(iscell(A), ...
+         'The input basis must be a cell containing tangent vectors at x');
 
-    n = numel(basis);
-    orthobasis = cell(size(basis));
+    n = numel(A);
+    R = zeros(n);
+    Q = cell(size(A));
     
-    % Build the Gram matrix of the basis vectors.
-    G = grammatrix(M, x, basis);
-    
-    % If the vectors in 'basis' were the columns of V, and the inner
-    % product were the classical dot product, then G = V'*V. We are looking
-    % for R, an invertible matrix such that V*R is orthogonal. Thus, R
-    % satisfies R'*V'*V*R = eye(n); equivalently:
-    %  G = inv(R)'*inv(R).
-    % Computing a Cholesky factorization of G yields L such that G = L'*L.
-    % Thus, R = inv(L). Each column of R states exactly which linear
-    % combinations of the vectors in 'basis' must be computed to produce
-    % the orthonormal basis.
-    %
-    % Of course, in that formalism, we could directly take a qr of V, but
-    % in the actual setting V is not available; the only simple object
-    % available is G.
-	%
-	% If this simple code turns out not to be satisfactory (most likely
-	% because of numerical instability), it may be good to consider
-	% implementing a modified Gram-Schmidt algorithm instead, and even to
-	% provide a helper function which calls it twice.
-    L = chol(G);
-    R = inv(L);
-    
-    % Note that R is upper triangular.
-    % We now compute the n linear combinations.
-    
-    for k = 1 : n
+    for j = 1 : n
         
-        orthobasis{k} = lincomb(M, x, basis(1:k), R(1:k, k));
+        v = A{j};
+        
+        for i = 1 : (j-1)
+           
+            qi = Q{i};
+            
+            R(i, j) = M.inner(x, qi, v);
+            
+            v = M.lincomb(x, 1, v, -R(i, j), qi);
+            
+        end
+        
+        R(j, j) = M.norm(x, v);
+        
+        Q{j} = M.lincomb(x, 1/R(j, j), v);
         
     end
 
