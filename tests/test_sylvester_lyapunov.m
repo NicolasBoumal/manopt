a81381fb622e2ea72949c4ba99fb4a50276df859
Nicolas Boumal <nicolasboumal@gmail.com>
2018-04-17 23:35:10 -0400

    quick test file for the new tool

---
+++ tests/test_sylvester_lyapunov.m
@@ -0,0 +1,19 @@
+clear; close all; clc;
+
+n = 500;
+complex = true;
+
+A = randn(n);
+X0 = randn(n);
+if complex
+    A = A + 1i*randn(n);
+    X0 = X0 + 1i*randn(n);
+end
+A = A+A';
+C = A*X0+X0*A;
+
+X1 = sylvester_symmetric(A, C);
+
+norm(A*X1+X1*A-C, 'fro') / norm(C, 'fro')
+norm(X0, 'fro')
+norm(X1, 'fro')
