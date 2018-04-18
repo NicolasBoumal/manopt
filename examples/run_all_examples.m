b93c8326c18a1b6c2dcace7ed3b059537bd1cd1b
Nicolas Boumal <nicolasboumal@gmail.com>
2018-04-18 09:53:53 -0400

    The test script was running itself too..

--- examples/run_all_examples.m
+++ examples/run_all_examples.m
@@ -3,5 +3,8 @@ list = ls('*.m');
 
 for k = 1 : size(list, 1)
     s = strtrim(list(k, :));
-    eval([s(1:end-2), ';']);
+    s = s(1:end-2); % remove '.m'
+    if ~strcmp(s, mfilename()) % don't run yourself
+        eval([s, ';']);
+    end
 end
