ceaa495628400138c0fb2007c03fb6049c3df44b
Nicolas Boumal <nicolasboumal@gmail.com>
2018-04-22 11:47:07 -0400

    Consolidating examples to have just 1 for both factories

--- examples/doubly_stochastic_denoising.m
+++ examples/doubly_stochastic_denoising.m
@@ -1,12 +1,11 @@
 function doubly_stochastic_denoising()
-
-% Given a matrix, find its closest, in the Frobenius norm, doubly 
-% stochastic matrix.
+% Find a doubly stochastic matrix closest to a given matrix, in Frobenius norm.
 %
-% function doubly_stochastic_denoising()
-%
-% This example demonstrates how to use the geometry factory for the
-% doubly stochastic multinomial manifold, dsmultinomialfactory.
+% This example demonstrates how to use the geometry factories for the
+% doubly stochastic multinomial manifold:
+%  multinomialdoublystochasticfactory and
+%  multinomialsymmetricfactory (for the symmetric case.)
+% 
 % The file is based on developments in the research paper
 % A. Douik and B. Hassibi, "Manifold Optimization Over the Set 
 % of Doubly Stochastic Matrices: A Second-Order Geometry"
@@ -15,72 +14,71 @@ function doubly_stochastic_denoising()
 % Link to the paper: https://arxiv.org/abs/1802.02628.
 %
 % Please cite the Manopt paper as well as the research paper:
-%     @Techreport{Douik2018Manifold,
-%       Title   = {Manifold Optimization Over the Set of Doubly Stochastic 
-%           Matrices: {A} Second-Order Geometry},
-%       Author  = {Douik, A. and Hassibi, B.},
-%       Journal = {Arxiv preprint ArXiv:1802.02628},
-%       Year    = {2018}
-%     }
+% @Techreport{Douik2018Manifold,
+%   Title   = {Manifold Optimization Over the Set of Doubly Stochastic 
+%              Matrices: {A} Second-Order Geometry},
+%   Author  = {Douik, A. and Hassibi, B.},
+%   Journal = {Arxiv preprint ArXiv:1802.02628},
+%   Year    = {2018}
+% }
+% 
 % This can be a starting point for many optimization problems of the form:
 %
-% minimize f(X) such that X is a doubly stochastic matrix.
-%
-% Note that the code is long because it showcases quite a few features of
-% Manopt: most of the code is optional.
+% minimize f(X) such that X is a doubly stochastic matrix (symmetric or not)
 %
 % Input:  None. This example file generates random data.
 % 
 % Output: None.
+%
 % This file is part of Manopt: www.manopt.org.
 % Original author: Ahmed Douik, March 15, 2018.
 % Contributors:
 % Change log:
-
-    close all
-    clear all
-    clc 
-    warning('off', 'manopt:dsmultinomialfactory:exp') 
-    % Clearing all variables, open windows, and disabiling all warnings 
-    % regarding the use of a retraction instead of the exponential map.
     
-    % Input data
-    n = 100 ; % Size of the matrix
-    sigma = 1/n^2 ; % Noise standard deviation at each entry
-
+    %% Generate input data
+    n = 100;
+    sigma = 1/n^2;
     % Generate a doubly stochastic matrix using the Sinkhorn algorithm
-    B = doubly_stochastic(abs(randn(n, n))) ; 
-    % Adding noise to the matrix
-    A = max(B + sigma*randn(n, n),0.01) ;
+    B = doubly_stochastic(abs(randn(n, n))); 
+    % Add noise to the matrix
+    A = max(B + sigma*randn(n, n), 0.01);
 
-    % Denoising function and derivatives
-    cost = @(X) (0.5*norm( A-X, 'fro')^2)  ; % Cost function
-    egrad = @(X) (X -A )  ;  % Euclidean Gradient
-    ehess = @(X, U) (U) ; % Euclidean Hessian
-
-    % Manifold initialization
-    manifold = dsmultinomialfactory(n) ;
+    %% Setup an optimization problem for denoising
+    
+    % Manifold initialization: pick the symmetric or non-symmetric case
+    symmetric_case = true;
+    if ~symmetric_case
+        manifold = multinomialdoublystochasticfactory(n);
+    else
+        % Input must also be symmetric (otherwhise, gradient must be adapted.)
+        A = (A+A')/2;
+        manifold = multinomialsymmetricfactory(n);
+    end
     problem.M = manifold;
-    problem.cost = cost;
-    problem.grad = @(X) manifold.egrad2rgrad(X, egrad(X));
-    problem.hess = @(X, U) manifold.ehess2rhess(X, egrad(X), ehess(X, U), U);
+    
+    % Specify cost function and derivatives
+    problem.cost = @(X) 0.5*norm(A-X, 'fro')^2;
+    problem.egrad = @(X) X - A;  % Euclidean gradient
+    problem.ehess = @(X, U) U;   % Euclidean Hessian
 
     % Check consistency of the gradient and the Hessian. Useful if you
     % adapt this example for a new cost function and you would like to make
     % sure there is no mistake.
-    figure() ;
+    warning('off', 'manopt:multinomialdoublystochasticfactory:exp');
+    warning('off', 'manopt:multinomialsymmetricfactory:exp');
+    figure();
     checkgradient(problem); % Check the gradient
-    figure() ;
+    figure();
     checkhessian(problem);  % Check the hessian. This test usually fails
                             % due to the use of a first order retraction, 
-                            % unless the test is performed at the optimum.
+                            % unless the test is performed at a critical point.
     
     % A first order algorithm
-    % Minimize the cost function using the Conjugate Gradient algorithm. 
+    % Minimize the cost function using the Conjugate Gradients algorithm.
     [X1, ~, info, ~] = conjugategradient(problem); 
     S1 = [info.gradnorm] ; % Collecting the Gradient information
     
-    % Computing the distance between the original, noisyless matrix and the
+    % Computing the distance between the original, noiseless matrix and the
     % found solution
     fprintf('||X-B||_F^2 = %g\n', norm(X1 - B, 'fro')^2);
 
@@ -89,38 +87,30 @@ function doubly_stochastic_denoising()
     [X2, ~, info, ~] = trustregions(problem);                                     
     S2 = [info.gradnorm] ; % Collecting the Gradient information
 
-    % Computing the distance between the original, noisyless matrix and the
+    % Computing the distance between the original, noiseless matrix and the
     % found solution
     fprintf('||X-B||_F^2 = %g\n', norm(X2 - B, 'fro')^2);
     
     figure() ;
-    loglog(S1,'red' ,'Marker','*','LineWidth',2,'DisplayName','Conjugate Gradient Algorithm') ; 
+    semilogy(S1, 'red', 'Marker', '*', 'LineWidth', 2, ...
+                 'DisplayName', 'Conjugate Gradient Algorithm');
     hold on ;
-    loglog(S2,'blue' ,'Marker','+','LineWidth',2,'DisplayName','Trust Regions Method') ; 
+    semilogy(S2, 'blue', 'Marker', '+', 'LineWidth', 2, ...
+                 'DisplayName', 'Trust Regions Method');
     
-    set(gca,'YGrid','on','XGrid','on') ;
-    xlabel('Number of Iteration','FontSize',16);
-    ylabel('Norm of Gradient','FontSize',16);
+    set(gca, 'YGrid', 'on', 'XGrid', 'on');
+    xlabel('Number of Iteration', 'FontSize', 16);
+    ylabel('Norm of Gradient', 'FontSize', 16);
     legend1 = legend('show');
-    set(legend1,'FontSize',16);
+    set(legend1, 'FontSize', 16);
 
-    % If the problem has a small enough dimension, we may (for analysis
-    % purposes) compute the spectrum of the Hessian at a point X. This may
-    % help in studying the conditioning of a problem. If you don't provide
-    % the Hessian, Manopt will approximate the Hessian with finite
-    % differences of the gradient and try to estimate its "spectrum" (it's
-    % not a proper linear operator). This can give some intuition, but
-    % should not be relied upon.
-    if problem.M.dim() < 100
-        fprintf('Computing the spectrum of the Hessian...');
-        s1 = hessianspectrum(problem, X1);
-        figure() ;
-        hist(s1);
-    end
-
-    figure()
-    checkhessian(problem,X1) % Checking the hessian at the optimal point. 
-                             % Notice that is the optimal solution has 
-                             % zeros, then it is not achievable by the 
-                             % manifold and the test is expected to fail.
+    % This Hessian test is performed at a computed solution, which is
+    % expected to be a critical point. Thus we expect the slope test to
+    % succeed. It could fail if the solution X1 has entries very close to
+    % zero, so that numerical issues might come up. This is because de
+    % Fisher metric on the multinomial manifold involves division by the
+    % entries of X.
+    figure();
+    checkhessian(problem, X1);
+    
 end
