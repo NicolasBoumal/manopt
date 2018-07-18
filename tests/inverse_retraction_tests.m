3976bd5b32a4234b60e64c15dd2953b439aa9ffd
Nicolas Boumal <nicolasboumal@gmail.com>
2018-07-18 15:27:27 +0200

    Test files for inverse retractions

---
+++ tests/inverse_retraction_tests.m
@@ -0,0 +1,21 @@
+clear all; close all; clc;
+
+% July 8, 2018, NB
+n = 5;
+p = 3;
+k = 2;
+M = stiefelfactory(n, p, k);
+
+M.retr = M.retr_qr;
+M.invretr = M.invretr_qr;
+
+% M.retr = M.retr_polar;
+% M.invretr = M.invretr_polar;
+
+X = M.rand();
+S = M.randvec(X);
+S = M.lincomb(X, randn(1), S);
+Y = M.retr(X, S);
+V = M.invretr(X, Y);
+fprintf('Retraction inversion error: %g\n', ...
+        M.norm(X, M.lincomb(X, 1, S, -1, V)));
