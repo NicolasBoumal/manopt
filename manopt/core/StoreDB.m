7e6ca710f5ec8d21411794907e39e5417281d200
Nicolas Boumal <nicolasboumal@gmail.com>
2018-08-02 15:41:34 -0400

    Core changes to StoreDB. Essentially two changes: (1) purge() now purges based on latest access time (access = read or write) rather than latest write time. (2) added functions remove() and removefirstifdifferent(), which makes it possible for solvers and line search algorithms to tell the storedb when a certain store is no longer relevant. For solvers that use the new remove() capabilities, purge() and storedepth have now very little importance. Furthermore, access is now more protected, with warnings issued if improper keys are accessed; this should make it easier to debug new solvers.

--- manopt/core/StoreDB.m
+++ manopt/core/StoreDB.m
@@ -14,6 +14,23 @@ classdef StoreDB < handle_light
 % Original author: Nicolas Boumal, April 3, 2015.
 % Contributors: 
 % Change log: 
+%
+%   Aug. 2, 2018 (NB)
+%       Purge now selects based on time since last access, whether read or
+%       write. It used to select based on last write. This should allow to
+%       set a smaller value of storedepth for trustregions (in particular).
+%       Also added StoreDB.remove(key) which allows to specifically remove
+%       the store associated to a key. This can be used by a solver when it
+%       is reasonably certain that the memory associated to a certain key
+%       is no longer needed (for example, when a step was rejected in
+%       trustregions or in a line-search algorithm.) Proper usage of this
+%       feature should allow to set a small value for storedepth.
+%       Furthermore, getNewKey() now places an empty store at the created
+%       key. As a result, it is no longer legitimate to call storedb.get on
+%       an unknown key: this now issues a warning, which should help debug.
+%       The function removefirstifdifferent(key1, key2) only removes key1
+%       if it is different from key2, which is helpful when an iteration
+%       may have failed to move to a distinct point.
 
 % TODO : protect get/setWithShared calls: limit to one, and forbid access
 %        to shared memory while it has not been returned.
@@ -65,14 +82,28 @@ classdef StoreDB < handle_light
             end
         end
         
-        % Return the store associated to a given key.
-        % If the key is unknown, returns an empty structure.
+        % Returns the store associated to a given key.
+        % If the key is unknown, issues a warning and returns empty store.
         function store = get(storedb, key)
             if isfield(storedb.history, key)
+                % Update access timer.
+                storedb.history.(key).lastaccess__ = storedb.timer;
+                % Extract the store.
                 store = storedb.history.(key);
             else
                 store = struct();
+                store.lastaccess__ = storedb.timer;
+                msg = 'Called storedb.get for a store with unknown key.';
+                % If the queried key is less than the counter, it must have
+                % been a valid key at some point. Inform the user that it
+                % may have been purged (or removed) prematurely.
+                if str2double(key(2:end)) < storedb.counter
+                    msg = [msg, ' It seems that key was purged or removed.'];
+                end
+                msg = [msg, ' Returned an empty structure.'];
+                warning('manopt:storedb:get', msg);
             end
+            storedb.timer = storedb.timer + 1;
         end
         
         % Same as get, but adds the shared memory in store.shared.
@@ -84,32 +115,61 @@ classdef StoreDB < handle_light
         % Save the given store at the given key. If no key is provided, a
         % new key is generated for this store (i.e., it is assumed this
         % store pertains to a new point). The key is returned in all cases.
-        % A field 'lastset__' is added/updated in the store structure,
-        % keeping track of the last time that store was modified.
+        % A field 'lastaccess__' is added/updated in the store structure,
+        % keeping track of the last time that store was accessed.
         function key = set(storedb, store, key)
             if nargin < 3
                 key = getNewKey(storedb);
             end
-            store.lastset__ = storedb.timer;
+            store.lastaccess__ = storedb.timer;
             storedb.timer = storedb.timer + 1;
             storedb.history.(key) = store;
         end
         
         % Same as set, but extracts the shared memory and saves it.
-        % The stored store will still have a 'shared' field, but it will be
-        % empty.
+        % The stored store will still have a 'shared' field, but empty.
         function key = setWithShared(storedb, store, key)
             storedb.shared = store.shared;
             store.shared = [];
             key = storedb.set(store, key);
         end
         
-        % Generate a unique key and return it. This should be called
+        % Erases a store from memory, identified by key.
+        % If the key is unknown, issues a warning.
+        function remove(storedb, key)
+            if isfield(storedb.history, key)
+                storedb.history = rmfield(storedb.history, key);
+            else
+                warning('manopt:storedb:remove', ...
+                       ['Attempted to remove store with unknown key.\n' ...
+                        'Perhaps it was purged? Try postponing purge().']);
+            end
+        end
+        
+        % Erases store at key1 if it is different from key2.
+        function removefirstifdifferent(storedb, key1, key2)
+            if ~strcmp(key1, key2)
+                storedb.remove(key1);
+            end
+        end
+        
+        % Generates a unique key and returns it. This should be called
         % everytime a new point is generated / stored. Keys are valid field
-        % names for structures.
+        % names for structures. After this call, an empty store is added in
+        % the cache at the newly generated key. It has minimal priority
+        % vis-à-vis storedb.purge().
         function key = getNewKey(storedb)
             key = sprintf('z%d', storedb.counter);
             storedb.counter = storedb.counter + 1;
+            % If we attempt to storedb.remove(key) or storedb.get(key) on
+            % this newly created key before anything is stored there, we
+            % will get a warning. Since there are legitimate scenarios for
+            % this to happen, we place an empty store at the new key
+            % immediately. Its last-access flag is set to 0 so it would be
+            % the first to be purged (unless it was later accessed.)
+            emptystore = struct();
+            emptystore.lastaccess__ = 0;
+            storedb.history.(key) = emptystore;
         end
         
         % Clear entries in storedb.history to limit memory usage.
@@ -131,21 +191,22 @@ classdef StoreDB < handle_light
             % If we need to remove some of the elements in the database,
             if nkeys > storedb.storedepth
 
-                % Get the last-set counter of each element:
-                % a higher number means it was modified more recently.
-                lastset = zeros(nkeys, 1, 'uint32');
+                % Get the last-access counter of each element:
+                % A higher number means it was accessed more recently.
+                % Both read and write operations are considered 'access'.
+                lastaccess = zeros(nkeys, 1, class(storedb.timer));
                 for i = 1 : nkeys
-                    lastset(i) = storedb.history.(keys{i}).lastset__;
+                    lastaccess(i) = storedb.history.(keys{i}).lastaccess__;
                 end
 
                 % Sort the counters and determine the threshold above which
                 % the field needs to be removed.
-                sortlastset = sort(lastset, 1, 'descend');
-                minlastset = sortlastset(storedb.storedepth);
+                sortlastaccess = sort(lastaccess, 1, 'descend');
+                minlastaccess = sortlastaccess(storedb.storedepth);
 
                 % Remove all fields that are too old.
                 storedb.history = rmfield(storedb.history, ...
-                                               keys(lastset < minlastset));
+                                         keys(lastaccess < minlastaccess));
             end
             
         end % end of purge()
