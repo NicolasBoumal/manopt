1a77b2a251644bfab5ddb9e3031fcceba8072924
Nicolas Boumal <nicolasboumal@gmail.com>
2018-08-03 18:55:46 -0400

    Grassmann now rolls on GPU -- and the example is adapted to work on it. (cosmetic changes to stiefel)

--- examples/using_gpu.m
+++ examples/using_gpu.m
@@ -6,7 +6,7 @@ function using_gpu()
 % there is a manifold you'd like to use on GPU, let us know via the forum
 % on http://www.manopt.org, we'll be happy to help!
 %
-% See also: spherefactory stiefelfactory complexcirclefactory
+% See also: spherefactory stiefelfactory grassmannfactory complexcirclefactory
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, Aug. 3, 2018.
@@ -25,21 +25,23 @@ function using_gpu()
     inner = @(U, V) U(:)'*V(:);
     
     % First, setup and run the optimization problem on the CPU.
-    problem.M = stiefelfactory(n, p, 1);  % 1 copy of Stiefel(n, p)
-    problem.cost = @(X) .5*inner(X, A*X); % Rayleigh quotient to be minimized
-    problem.egrad = @(X) A*X;             % Could use caching to save here
-    X0 = problem.M.rand();                % Random initial guess
+    problem.M = grassmannfactory(n, p, 1); % 1 copy of Grassmann(n, p)
+    problem.cost = @(X) .5*inner(X, A*X);  % Rayleigh quotient to be minimized
+    problem.egrad = @(X) A*X;              % Could use caching to save here
+    problem.ehess = @(X, Xdot) A*Xdot;
+    X0 = problem.M.rand();                 % Random initial guess
     tic_cpu = tic();
-    X_cpu = trustregions(problem, X0); % run any solver
+    X_cpu = trustregions(problem, X0);     % run any solver
     time_cpu = toc(tic_cpu);
     
     % Then, move the data to the GPU, redefine the problem using the moved
     % data, activate the GPU flag in the factory, and run it again.
     A = gpuArray(A);
-    problem.M = stiefelfactory(n, p, 1, true); % true is the GPU flag;
-    problem.cost = @(X) .5*inner(X, A*X);      % Code for cost and gradient
-    problem.egrad = @(X) A*X;                  % basically didn't change, but
-    X0 = gpuArray(X0);                         % operates on gpuArrays now.
+    problem.M = grassmannfactory(n, p, 1, true); % true is the GPU flag;
+    problem.cost = @(X) .5*inner(X, A*X);        % Code for cost and gradient etc.
+    problem.egrad = @(X) A*X;                    % basically didn't change, but
+    problem.ehess = @(X, Xdot) A*Xdot;           % operates on gpuArrays now.
+    X0 = gpuArray(X0);
     tic_gpu = tic();
     X_gpu = trustregions(problem, X0);
     time_gpu = toc(tic_gpu);
