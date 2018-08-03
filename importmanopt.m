032acd1ff697f90ad76a91574e270b81ac0805c5
Nicolas Boumal <nicolasboumal@gmail.com>
2018-08-03 18:35:52 -0400

    Now checking if savepath succeeded, and showing an error message if it did not.

--- importmanopt.m
+++ importmanopt.m
@@ -21,8 +21,14 @@ cd ..;
 fprintf('Manopt was added to Matlab''s path.\n');
 response = input('Save path for future Matlab sessions? [Y/N] ', 's');
 if strcmpi(response, 'Y')
-    savepath();    
-    fprintf('Path saved: no need to call importmanopt next time.\n');
+    failed = savepath();
+    if ~failed
+        fprintf('Path saved: no need to call importmanopt next time.\n');
+    else
+        fprintf(['Something went wrong.. Perhaps missing permission ' ...
+                 'to write on pathdef.m?\nPath not saved: ' ...
+                 'please re-call importmanopt next time.\n']);
+    end
 else
-    fprintf('Path not saved: please call importmanopt next time.\n');
+    fprintf('Path not saved: please re-call importmanopt next time.\n');
 end
