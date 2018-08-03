cc2d62a6066bfb7dd9401ae04f5547d8bf3294c2
Nicolas Boumal <nicolasboumal@gmail.com>
2018-08-03 18:22:00 -0400

    Changed GPU example to be on Stiefel

--- examples/using_gpu.m
+++ examples/using_gpu.m
@@ -6,7 +6,7 @@ function using_gpu()
 % there is a manifold you'd like to use on GPU, let us know via the forum
 % on http://www.manopt.org, we'll be happy to help!
 %
-% See also: spherefactory complexcirclefactory
+% See also: spherefactory stiefelfactory complexcirclefactory
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, Aug. 3, 2018.
@@ -14,34 +14,37 @@ function using_gpu()
 % Change log: 
 
     % Construct a large problem to illustrate the use of GPU.
-    % Below, we will compute a left-most eigenvector of A (symmetric).
+    % Below, we will compute p left-most eigenvectors of A (symmetric).
     % On a particular test computer, we found that for n = 100, 1000, CPU
     % is faster, but for n = 10000, GPU tends to be 10x faster.
+    p = 3;
     n = 10000;
     A = randn(n);
-    A = .5*(A+A');
+    A = A+A';
+    
+    inner = @(U, V) U(:)'*V(:);
     
     % First, setup and run the optimization problem on the CPU.
-    problem.M = spherefactory(n, 1);   % sphere: nx1 vectors of norm 1
-    problem.cost = @(x) .5*x'*(A*x);   % Rayleigh quotient to be minimized
-    problem.egrad = @(x) A*x;          % could use caching to save here
-    x0 = problem.M.rand();             % random initial guess
+    problem.M = stiefelfactory(n, p, 1);  % 1 copy of Stiefel(n, p)
+    problem.cost = @(X) .5*inner(X, A*X); % Rayleigh quotient to be minimized
+    problem.egrad = @(X) A*X;             % Could use caching to save here
+    X0 = problem.M.rand();                % Random initial guess
     tic_cpu = tic();
-    x_cpu = trustregions(problem, x0); % run any solver
+    X_cpu = trustregions(problem, X0); % run any solver
     time_cpu = toc(tic_cpu);
     
     % Then, move the data to the GPU, redefine the problem using the moved
     % data, activate the GPU flag in the factory, and run it again.
     A = gpuArray(A);
-    problem.M = spherefactory(n, 1, true); % true is the GPU flag
-    problem.cost = @(x) .5*x'*(A*x);       % code for cost and gradient are
-    problem.egrad = @(x) A*x;              % basically the same, but operate
-    x0 = gpuArray(x0);                     % on gpuArrays now.
+    problem.M = stiefelfactory(n, p, 1, true); % true is the GPU flag;
+    problem.cost = @(X) .5*inner(X, A*X);      % Code for cost and gradient
+    problem.egrad = @(X) A*X;                  % basically didn't change, but
+    X0 = gpuArray(X0);                         % operates on gpuArrays now.
     tic_gpu = tic();
-    x_gpu = trustregions(problem, x0);
+    X_gpu = trustregions(problem, X0);
     time_gpu = toc(tic_gpu);
     
     fprintf('Total time CPU: %g\nTotal time GPU: %g\nSolution difference: %g\n', ...
-            time_cpu, time_gpu, norm(x_cpu - x_gpu, 'fro'));
+            time_cpu, time_gpu, norm(X_cpu - X_gpu, 'fro'));
     
 end
