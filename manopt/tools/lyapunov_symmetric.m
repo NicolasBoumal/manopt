f6b6ab9a7ae0d7d81219ea522a58317db411764e
Nicolas Boumal <nicolasboumal@gmail.com>
2018-08-31 10:01:59 -0400

    lyapunov_symmetric now allows solving more than one system with the same matrix A at the same time, which is more efficient. This is done by adding the new tool lyapunov_symmetric_eig, which takes as input the eigendecomposition of A directly. This further allows solving nested systems efficiently, as is now done in the fixedrankfactory_2factors_subspace_projection factory's projection.

--- manopt/tools/lyapunov_symmetric.m
+++ manopt/tools/lyapunov_symmetric.m
@@ -4,9 +4,14 @@ function X = lyapunov_symmetric(A, C, tol)
 % function X = lyapunov_symmetric(A, C)
 % function X = lyapunov_symmetric(A, C, tol)
 %
-% When the solution exists and is unique, this is equivalent to
-% sylvester(A, A', C) or lyap(A, -C). This works for both real and complex
-% inputs.
+% Matrices A, C and X have size nxn. When the solution exists and is
+% unique, this is equivalent to sylvester(A, A', C) or lyap(A, -C).
+% This works for both real and complex inputs.
+%
+% If C is a 3-D array of size nxnxk, then X has size nxnxk as well, and
+% each slice X(:, :, i) corresponds to the solution for the system with
+% right-hand side C(:, :, i). This is more efficient then calling the
+% function multiple times for each slice of C.
 % 
 % If the solution is not unique, the smallest-norm solution is returned.
 %
@@ -20,13 +25,32 @@ function X = lyapunov_symmetric(A, C, tol)
 % but it is computed far more efficiently. The most expensive step is an
 % eigendecomposition of A, whose complexity is essentially O(n^3) flops.
 %
+% If A is not symmetric, only its symmetric part is used: (A+A')/2.
+%
 % If C is (skew-)symmetric, then X is (skew-)symmetric (up to round-off),
 % and similarly in the complex case.
+%
+% To solve one system at a time, while reusing the eigendecomposition of A,
+% call lyapunov_symmetric_eig.
+%
+% See also: lyapunov_symmetric_eig sylvester lyap sylvester_nochecks
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, April 17, 2018.
 % Contributors: 
 % Change log: 
+%   Aug. 31, 2018 (NB):
+%       Now works with C having multiple slices (nxnxk), and added some
+%       safeguards.
+
+    n = size(A, 1);
+    assert(ismatrix(A) && size(A, 2) == n, 'A must be square.');
+    assert(size(C, 1) == n && size(C, 2) == n, ...
+           'Each slice of C must have the same size as A.');
+       
+   if ~exist('tol', 'var')
+       tol = [];
+   end
 
     % Make sure A is numerically Hermitian (or symmetric).
     % The cost of this safety step is negligible compared to eig.
@@ -35,25 +59,11 @@ function X = lyapunov_symmetric(A, C, tol)
     % V is unitary or orthogonal and lambda is real.
     [V, lambda] = eig(A, 'vector');
     
-    % AX + XA = C  is equivalent to DY + YD = M with
-    % Y = V'XV, M = V'CV and D = diag(lambda).
-    M = V'*C*V;
-    
-    % W(i, j) = lambda(i) + lambda(j)
-    W = bsxfun(@plus, lambda, lambda');
-    
-    % Normally, the solution Y is simply this:
-    Y = M ./ W;
-    
-    % But this may involve divisions by (almost) 0 in certain places.
-    % Thus, we go for a pseudo-inverse.
-    absW = abs(W);
-    if ~exist('tol', 'var') || isempty(tol)
-        tol = numel(A)*eps(max(absW(:))); % similar to pinv tolerance
+    % Solve for each slice separately.
+    X = zeros(size(C));
+    for k = 1 : size(C, 3)
+        X(:, :, k) = lyapunov_symmetric_eig(V, lambda, C(:, :, k), tol);
     end
-    Y(absW <= tol) = 0;
-    
-    % Undo the change of variable
-    X = V*Y*V';
 
 end
+
