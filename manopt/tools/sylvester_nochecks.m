dfd541b22ece8a89d9d5f72d46954e00cc420717
Nicolas Boumal <nicolasboumal@gmail.com>
2018-09-06 21:27:00 -0400

    added ref to lyapunov tool

--- manopt/tools/sylvester_nochecks.m
+++ manopt/tools/sylvester_nochecks.m
@@ -3,15 +3,15 @@ function [X, unique] = sylvester_nochecks(A, B, C)
 %
 % function [X, notunique] = sylvester_nochecks(A, B, C)
 %
-% Solves the Sylvester equation A*X + X*B = C, where A is a m-by-m matrix,
-% B is a n-by-n matrix, and X and C are two m-by-n matrices. The returned
+% Solves the Sylvester equation A*X + X*B = C, where A is an m-by-m matrix,
+% B is an n-by-n matrix, and X and C are two m-by-n matrices. The returned
 % flag 'unique' is set to false if the solution is not unique.
 %
 % This is a stripped-down version of Matlab's own sylvester function that
 % bypasses any input checks. This is significantly faster for small m and
 % n, which is often useful in Manopt.
 %
-% See also: sylvester
+% See also: sylvester lyapunov_symmetric
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Nicolas Boumal, July 19, 2018
