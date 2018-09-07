133bc0f83edfe64a2a726363f2c3d8cea4f83c5e
Nicolas Boumal <nicolasboumal@gmail.com>
2018-09-06 22:22:07 -0400

    cosmetic (the erased statement was duplicate in the file)

--- manopt/manifolds/essential/essentialfactory.m
+++ manopt/manifolds/essential/essentialfactory.m
@@ -176,11 +176,6 @@ function M = essentialfactory(k, strSigned)
         % Q is a [3x6] matrix where each [3x3] block is a uniformly distributed
         % matrix.
         
-        % This file is part of Manopt: www.manopt.org.
-        % Original author: Roberto Tron, Aug. 8, 2014
-        % Contributors:
-        % Change log:
-        
         if nargin < 1
             N = 1;
         end
