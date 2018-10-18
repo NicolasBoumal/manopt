b40339bc54bb8f0f385de34d829d0c6044410b11
Nicolas Boumal <nicolasboumal@gmail.com>
2018-10-18 10:58:15 -0400

    turning the reference generation script into a function (and disabling graph generation)

--- reference/generate_manopt_reference.m
+++ reference/generate_manopt_reference.m
@@ -1,37 +1,40 @@
-clear all;
-close all;
-clc;
+function generate_manopt_reference(target_html_dir_relative_to_manopt_root)
 
-% Add m2html tool to the Matlab path
-cd m2html/;
-addpath(pwd);
-cd ..;
+    % This code assumes it's run from its own directory; could be improved
+    % using mfilename('fullpath') and pwd() for example.
 
-% Go back to the main directory hosting manopt files.
-cd ..;
+    % Add m2html tool to the Matlab path
+    cd m2html/;
+    addpath(pwd());
+    cd ..;
 
-% Setup options
-options = struct();
-options.verbose = 'on';
-options.mFiles = {'manopt', 'examples'};
-options.htmlDir = 'web/reference/';
-options.recursive = 'on';
-options.source = 'on';
-options.download = 'off';
-options.syntaxHighlighting = 'on';
-options.tabs = 4;
-options.globalHypertextLinks = 'on';
-options.graph = 'on';
-options.todo = 'off';
-% options.load = 0;
-% options.save = 'off';
-options.search = 'off';
-options.helptocxml = 'off';
-options.indexFile = 'index';
-options.extension = '.html';
-options.template = 'blue';
-options.ignoredDir = {'.svn', 'cvs', '.git'};
-options.language = 'english';
+    % Go back to the main directory hosting manopt files.
+    cd ..;
 
-% Call the magician
-m2html(options);
+    % Setup options
+    options = struct();
+    options.verbose = 'on';
+    options.mFiles = {'manopt', 'examples'};
+    options.htmlDir = target_html_dir_relative_to_manopt_root;
+    options.recursive = 'on';
+    options.source = 'on';
+    options.download = 'off';
+    options.syntaxHighlighting = 'on';
+    options.tabs = 4;
+    options.globalHypertextLinks = 'on';
+    options.graph = 'off';
+    options.todo = 'off';
+    % options.load = 0;
+    % options.save = 'off';
+    options.search = 'off';
+    options.helptocxml = 'off';
+    options.indexFile = 'index';
+    options.extension = '.html';
+    options.template = 'blue';
+    options.ignoredDir = {'.svn', 'cvs', '.git'};
+    options.language = 'english';
+
+    % Call the magician
+    m2html(options);
+
+end
