3ba02b8385b268116111385939bec2822565a3b6
Nicolas Boumal <nicolasboumal@gmail.com>
2018-12-16 18:30:53 -0500

    test files for a new retraction for generalized Stiefel

---
+++ tests/test_generalized_stiefel_retraction.m
@@ -0,0 +1,37 @@
+% Dec. 16, 2018, NB
+% Following up on "Cholesky QR-based retraction on the generalized Stiefel
+% manifold" by Sato and Aihara, https://doi.org/10.1007/s10589-018-0046-7
+%
+
+clear all; close all; clc;
+
+% The Cholesky baed approach might have numerical issues due to squaring,
+% which leads to squaring the condition number. A modified Gram Schmidt
+% executed directly in Euclidean space with G inner product resolve that
+% issue (though admittedly it'd be a lot slower unless there is a C-mex
+% version of it.)
+
+n = 100;
+p = 5;
+
+F = randn(n);
+B = F'*F;
+
+M = stiefelgeneralizedfactory_HS_KA(n, p, B);
+X = M.rand();
+U = M.randvec(X);
+
+Y = M.retr(X, U);
+norm(Y'*B*Y - eye(p))
+
+% Modified Gram-Schmidt: input is a point X on the manifold and a tangent
+% vector U at X. Output is Q = Retr_X(U).
+Q_MGS  = stiefelgeneralized_retraction_MGS(B, X, U);
+Q_MGS2 = stiefelgeneralized_retraction_MGS_twice(B, X, U);
+
+% Mathematically, this Q corresponds to the "Cholesky QR" retraction,
+% except it never squares the condition number, so it should in principle
+% be numerically more robust (though it's probably slower because of the
+% loop in Matlab.)
+norm(Q_MGS'*B*Q_MGS - eye(p))
+norm(Q_MGS2'*B*Q_MGS2 - eye(p))
