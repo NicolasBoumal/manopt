3edcae99ded0b27c765fdd1521a959af929bce95
Nicolas Boumal <nicolasboumal@gmail.com>
2018-12-27 14:37:34 +0100

    Added a new retraction to generalized Stiefel manifold, and made it the default retraction (contribution of Hiroyuki Sato and Kensuke Aihara)

--- manopt/manifolds/stiefel/stiefelgeneralizedfactory.m
+++ manopt/manifolds/stiefel/stiefelgeneralizedfactory.m
@@ -22,18 +22,27 @@ function M = stiefelgeneralizedfactory(n, p, B)
 % this is a bottleneck for a specific application, then a way forward is to
 % create a modified version of this file which preprocesses B to speed this
 % up (typically, by computing a Cholesky factorization of it, then calling
-% an appropriate solver).
+% an appropriate solver; or by exploiting sparsity.)
 %
 % See also: stiefelfactory  grassmannfactory  grassmanngeneralizedfactory 
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Bamdev Mishra, June 30, 2015.
-% Contributors:
+% Contributors: Hiroyuki Sato and Kensuke Aihara, Dec. 27, 2018.
 %
 % Change log:
 %
-%    Sep. 6, 2018 (NB):
+%    Sep.  6, 2018 (NB):
 %        Removed M.exp() as it was not implemented.
+%
+%    Dec. 27, 2018 (NB):
+%        Added retraction M.retr_qr (based on QR factorization) as an
+%        alternative to the previous retraction, which is now accessible as
+%        M.retr_polar. The new retraction should be more efficient: it is
+%        now the default, as M.retr = M.retr_qr. This new retraction is a
+%        contribution of H. Sato and K. Aihara, as per their paper:
+%        'Cholesky QR-based retraction on the generalized Stiefel manifold'
+%        https://link.springer.com/article/10.1007%2Fs10589-018-0046-7
 
     
     if ~exist('B', 'var') || isempty(B)
@@ -68,7 +77,8 @@ function M = stiefelgeneralizedfactory(n, p, B)
     function rgrad = egrad2rgrad(X, egrad)
         
         % First, scale egrad according the to the scaled metric in the
-        % Euclidean space.
+        % Euclidean space. Ideally, B should be preprocessed to ease
+        % solving linear systems, e.g., via Cholesky factorization.
         egrad_scaled = B\egrad;
         
         % Second, project onto the tangent space.
@@ -88,29 +98,40 @@ function M = stiefelgeneralizedfactory(n, p, B)
         
         % Directional derivative of the Riemannian gradient.
         egrad_scaleddot = B\egraddot;
-        rgraddot = egrad_scaleddot - Xdot*symm(X'*egrad)...
-            - X*symm(Xdot'*egrad)...
-            - X*symm(X'*egraddot);
+        rgraddot = egrad_scaleddot - Xdot*symm(X'*egrad) ...
+                                   - X*symm(Xdot'*egrad) ...
+                                   - X*symm(X'*egraddot);
         
         % Project onto the tangent space.
         rhess = M.proj(X, rgraddot);
     end
     
     
-    M.retr = @retraction;
-    function Y = retraction(X, U, t)
+    M.retr_polar = @retraction_polar;
+    function Y = retraction_polar(X, U, t)
         if nargin < 3
             t = 1.0;
         end
-        Y = guf(X + t*U); % Ensure that Y'*B*Y is identity.
+        Y = guf(X + t*U); % Ensures Y'*B*Y is identity.
     end
 
+    M.retr_qr = @retraction_qr;
+    function Y = retraction_qr(X, U, t)
+        if nargin < 3
+            t = 1.0;
+        end
+        Y = gqf(X + t*U); % Ensures Y'*B*Y is identity.
+    end
+
+    % By default, we use the QR retraction
+    M.retr = M.retr_qr;
 
     M.hash = @(X) ['z' hashmd5(X(:))];
     
     M.rand = @random;
     function X = random()
-        X = guf(randn(n, p)); % Ensure that X'*B*X is identity;
+        X = guf(randn(n, p)); % Ensures X'*B*X is identity;
+        % TODO: test if this can be replaced by gqf.
     end
     
     M.randvec = @randomvec;
@@ -133,6 +154,37 @@ function M = stiefelgeneralizedfactory(n, p, B)
     % Some auxiliary functions
     symm = @(D) (D + D')/2;
     
+
+    function X = gqf(Y)
+        % Generalized QR decomposition of an n-by-p matrix Y.
+        % X'*B*X is identity. See Sato and Aihara 2018,
+        % Cholesky QR-based retraction on the generalized Stiefel manifold,
+        % https://link.springer.com/article/10.1007%2Fs10589-018-0046-7
+        %
+        % Comment by Nicolas Boumal, Dec. 27, 2018, following discussions
+        % with Hiroyuki Sato, Kensuke Aihara and Bamdev Mishra:
+        %
+        % In principle, to orthonormalize the columns of Y against the
+        % inner product defined by B, it would be better to implement (for
+        % example) a modified Gram-Schmidt algorithm, possibly run twice.
+        % Indeed, the latter would be affected by the condition number of
+        % sqrtm(B)*Y. In contrast, the method below first squares the data,
+        % hence is affected by the condition number of Y'*B*Y, which is the
+        % square of that of sqrtm(B)*Y. Fortunately, it is easily shown
+        % that, when Y = X + U for a tangent vector U at X, the condition
+        % number of sqrtm(B)*Y is upper bounded by the square root of
+        % 1 + ||U||_X^2. In Riemannian optimization, it is seldom necessary
+        % to retract very large vectors, hence it is expected that the
+        % condition numbers encountered in practice will be reasonable. As
+        % a result, the implementation below which calls directly upon
+        % low-level routines (Cholesky and a small triangular system solve)
+        % is expected to run faster in practice than a home-made
+        % implementation of modified Gram-Schmidt, which would involve a
+        % loop, and still be sufficiently accurate.
+        R = chol(symm(Y'*(B*Y)));
+        X = Y / R;
+    end
+
     function X = guf(Y)
         % Generalized polar decomposition of an n-by-p matrix Y.
         % X'*B*X is identity.
@@ -153,7 +205,7 @@ function M = stiefelgeneralizedfactory(n, p, B)
         
     end
     
-    function [u, s, v] = restricted_svd(Y)
+    function [u, s, v] = restricted_svd(Y) %#ok<DEFNU>
         % We compute a thin svd-like decomposition of an n-by-p matrix Y 
         % into matrices u, s, and v such that u is an n-by-p matrix
         % with u'*B*u being identity, s is a p-by-p diagonal matrix 
