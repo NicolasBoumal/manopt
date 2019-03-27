7d605a5d77beec7f4fd29f45e173049053deab18
Bamdev Mishra <bamdevm@gmail.com>
2019-03-27 17:36:05 +0530

    create a factory for fixed sparstity euclidean matrices
    Euclidean matrices with fixed sparsity patttern.

---
+++ tests/bmsparseeuclidean/sparseeuclideanfactory.m
@@ -0,0 +1,80 @@
+function M = sparseeuclideanfactory(A)
+% Returns a manifold struct to optimize over real matrices with sparsity pattern that of A.
+%
+% function M = sparseeuclideanfactory(A)
+%
+% Returns M, a structure describing the Euclidean space of real matrices,
+% equipped with the standard Frobenius distance and associated trace inner
+% product, as a manifold for Manopt.
+%
+% See also: euclideanfactory euclideancomplexfactory
+
+% This file is part of Manopt: www.manopt.org.
+% Original author: Bamdev Mishra, Mar. 28, 2019.
+% Change log: 
+%
+	
+	dimensions_vec = size(A);
+	[I,J] = find(A);
+	S = sparse(I, J, ones(length(I),1), dimensions_vec(1), dimensions_vec(2));
+	nvals = length(I);
+      
+    M.size = @() dimensions_vec;
+    
+    M.name = @() sprintf('Euclidean space R^(%s) with fixed sparsity pattern containg %s non-zero entries', num2str(dimensions_vec), num2str(nvals));
+    
+    M.dim = @() nvals;
+    
+    M.inner = @(x, d1, d2) d1(:).'*d2(:);
+    
+    M.norm = @(x, d) norm(d(:), 'fro');
+    
+    M.dist = @(x, y) norm(x(:) - y(:), 'fro');
+    
+    M.typicaldist = @() sqrt(prod(dimensions_vec));
+    
+    M.proj = @(x, d) d;
+    
+    M.egrad2rgrad = @(x, g) S.*g;
+    
+    M.ehess2rhess = @(x, eg, eh, d) S.*eh;
+    
+    M.tangent = M.proj;
+    
+    M.exp = @exp;
+    function y = exp(x, d, t)
+        if nargin == 3
+            y = x + t*d;
+        else
+            y = x + d;
+        end
+    end
+    
+    M.retr = M.exp;
+	
+	M.log = @(x, y) y-x;
+
+    M.hash = @(x) ['z' hashmd5(x(:))];
+    
+    M.rand = @() S.*randn(dimensions_vec);
+    
+    M.randvec = @randvec;
+    function u = randvec(x) %#ok<INUSD>
+        u = S.*randn(dimensions_vec);
+        u = u / norm(u(:), 'fro');
+    end
+    
+    M.lincomb = @matrixlincomb;
+    
+    M.zerovec = @(x) zeros(dimensions_vec);
+    
+    M.transp = @(x1, x2, d) d;
+    M.isotransp = M.transp; % the transport is isometric
+    
+    M.pairmean = @(x1, x2) .5*(x1+x2);
+    
+    M.vec = @(x, u_mat) u_mat(:);
+    M.mat = @(x, u_vec) reshape(u_vec, dimensions_vec);
+    M.vecmatareisometries = @() true;
+
+end
