d27e86e63c43c076ea85ff51f21abb647542315e
Nicolas Boumal <nicolasboumal@gmail.com>
2019-06-15 10:25:25 -0400

    Included a word about the adjoint of dfunm in all files

--- manopt/tools/dlogm.m
+++ manopt/tools/dlogm.m
@@ -9,6 +9,10 @@ function [D, fX] = dlogm(X, H)
 % Thus, D = lim_(t -> 0) (logm(X + tH) - logm(X)) / t.
 %
 % The second output is fX = logm(X), though it may be less accurate.
+%
+% Note: the adjoint of dlogm(X, .) is dlogm(X', .), which is a fact often
+% useful to derive gradients of matrix functions involving logm(X).
+% (This is wrt the inner product inner = @(A, B) real(trace(A'*B))).
 % 
 % See also: dfunm dexpm dsqrtm
 
--- manopt/tools/dsqrtm.m
+++ manopt/tools/dsqrtm.m
@@ -9,6 +9,10 @@ function [D, fX] = dsqrtm(X, H)
 % Thus, D = lim_(t -> 0) (sqrtm(X + tH) - sqrtm(X)) / t.
 %
 % The second output is fX = sqrtm(X), though it may be less accurate.
+%
+% Note: the adjoint of dsqrtm(X, .) is dsqrtm(X', .), which is a fact often
+% useful to derive gradients of matrix functions involving sqrtm(X).
+% (This is wrt the inner product inner = @(A, B) real(trace(A'*B))).
 % 
 % See also: dfunm dlogm dexpm
 
