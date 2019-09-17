44c5f03b225d2b305e62b701d0e8c6cd3bbab881
Bamdev Mishra <bamdevm@gmail.com>
2019-09-17 09:33:25 +0530

    documentation
    Adding description on the metric

--- tests/bmtestspolar/symfixedrankpolarfactory.m
+++ tests/bmtestspolar/symfixedrankpolarfactory.m
@@ -58,7 +58,9 @@ function M = symfixedrankpolarfactory(m, k)
     M.dim = @() m*k - k*(k-1)/2;
     
     % Choice of the metric on the orthonormal space is motivated by the symmetry present in the
-    % space. The metric on the positive definite space is its natural metric.
+    % space. The metric on the positive definite space is its natural 
+    % bi-invarint metric. The metric on the Stiefel manifold is the
+    % standard Euclidean metric.
     M.inner = @(X, eta, zeta) eta.U(:).'*zeta.U(:)  ...
         + trace( (X.B\eta.B) * (X.B\zeta.B) );
     
