54c8ec64aaaa21f121a5fe8ca10740fd67419333
Nicolas Boumal <nicolasboumal@gmail.com>
2020-07-30 14:51:13 +0200

    Fixed sylvester_nochecks for more recent Matlab versions

--- manopt/tools/sylvester_nochecks.m
+++ manopt/tools/sylvester_nochecks.m
@@ -1,11 +1,10 @@
-function [X, unique] = sylvester_nochecks(A, B, C)
+function X = sylvester_nochecks(A, B, C)
 % Solve Sylvester equation without input checks.
 %
-% function [X, notunique] = sylvester_nochecks(A, B, C)
+% function X = sylvester_nochecks(A, B, C)
 %
 % Solves the Sylvester equation A*X + X*B = C, where A is an m-by-m matrix,
-% B is an n-by-n matrix, and X and C are two m-by-n matrices. The returned
-% flag 'unique' is set to false if the solution is not unique.
+% B is an n-by-n matrix, and X and C are two m-by-n matrices.
 %
 % This is a stripped-down version of Matlab's own sylvester function that
 % bypasses any input checks. This is significantly faster for small m and
@@ -17,6 +16,11 @@ function [X, unique] = sylvester_nochecks(A, B, C)
 % Original author: Nicolas Boumal, July 19, 2018
 % Contributors: This is a modification of Matlab's built-in sylvester.
 % Change log: 
+%   July 30, 2020 (NB):
+%       Changed call from builtin('_sylvester_tri', ...) to
+%       matlab.internal.math.sylvester_tri(...), which seems necessary for
+%       more recent versions of Matlab.
+%       Also had to remove the second output: the 'unique' flag.
 
     flag = 'real';
     if ~isreal(A) || ~isreal(B) || ~isreal(C)
@@ -27,9 +31,10 @@ function [X, unique] = sylvester_nochecks(A, B, C)
     [QB, TB] = schur(B, flag);
     
     % Solve Sylvester Equation TA*Y + Y*TB = QA'*C*QB.
-    [Y, info] = builtin('_sylvester_tri', TA, TB, QA'*C*QB);
-    
-    unique = (info ~= 1);
+    Y = matlab.internal.math.sylvester_tri(TA, TB, QA'*C*QB, ...
+                                                     'I', 'I', 'notransp');
+    % Use this call instead for older versions of Matlab:
+    % [Y, info] = builtin('_sylvester_tri', TA, TB, QA'*C*QB);
 
     X = QA*Y*QB';
     
