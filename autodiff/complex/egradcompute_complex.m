a72b6a54d09720defce1c99c5a9cd483fb40e664
Xiaowen Jiang <71407346+Xiaowen-Jiang@users.noreply.github.com>
2021-08-20 10:00:11 +0800

    Delete autodiff/complex directory

--- autodiff/complex/egradcompute_complex.m
+++
@@ -1,7 +0,0 @@
-function egrad = egradcompute_complex(autogradfunc,x)
-    % check 
-    dlx = mat2dl_complex(x);
-    [f,egrad] = dlfeval(autogradfunc,dlx);
-    % f = dl2mat(f);
-    egrad = dl2mat_complex(egrad);
-end
