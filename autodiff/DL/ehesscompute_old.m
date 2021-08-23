cab357e448b5ba22bfc4e0304a50f1f19daeb1fb
Xiaowen Jiang <71407346+Xiaowen-Jiang@users.noreply.github.com>
2021-08-23 20:55:43 +0800

    Delete ehesscompute_old.m

--- autodiff/DL/ehesscompute_old.m
+++
@@ -1,8 +0,0 @@
-function ehess = ehesscompute(autohessfunc,x,xdot)
-    
-    dlx = mat2dl(x);
-    dlxdot = mat2dl(xdot);
-    ehess = dlfeval(autohessfunc,dlx,dlxdot);
-    ehess = dl2mat(ehess);
-
-end
