a233643f9a906223396550ca0a4d01a36cadbddc
Xiaowen Jiang <71407346+Xiaowen-Jiang@users.noreply.github.com>
2021-08-23 20:57:34 +0800

    Delete computecostgrad.m

--- autodiff/DL/computecostgrad.m
+++
@@ -1,7 +0,0 @@
-function [f, g] = computecostgrad(autogradfunc, problem, x)
-    dlx = mat2dl(x);
-    [f,egrad] = dlfeval(autogradfunc,dlx);
-    f = dl2mat(f);
-    egrad = dl2mat(egrad);
-    g = problem.M.egrad2rgrad(x,egrad);
-end
