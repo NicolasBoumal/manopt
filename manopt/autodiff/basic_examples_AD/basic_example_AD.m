439f57379c707341eb8e31bab282e2925a76ecbc
Xiaowen Jiang <71407346+Xiaowen-Jiang@users.noreply.github.com>
2021-08-28 19:50:26 +0200

    Add files via upload

---
+++ manopt/autodiff/basic_examples_AD/basic_example_AD.m
@@ -0,0 +1,75 @@
+function basic_example_AD()
+% A basic example that shows how to apply automatic differentiation to
+% computing the gradient and the hessian.
+%
+% Note: Computation of the hessian is not available via AD for Matlab 
+% version R2020b or earlier. To fully exploit the convenience of AD,
+% please update to R2021b or later if possible.
+% 
+%
+% See also: preprocessAD, functions_AD.m 
+
+% This file is part of Manopt and is copyrighted. See the license file.
+%
+% Main author: Xiaowen Jiang, August, 31, 2021
+% Contributors: Nicolas Boumal
+% Change log:
+%    
+
+    % Verify that Manopt was indeed added to the Matlab path.
+    if isempty(which('spherefactory'))
+        error(['You should first add Manopt to the Matlab path.\n' ...
+		       'Please run importmanopt.']);
+    end
+    
+    % Verify that the deep learning tool box was installed
+    assert(exist('dlarray', 'file') == 2, ['Deep learning tool box is '... 
+    'needed for automatic differentiation.\n Please install the'...
+    'latest version of the deep learning tool box and \nupgrade to Matlab'...
+    ' 2021a if possible.'])
+    
+    % Generate the problem data.
+    n = 1000;
+    A = randn(n);
+    A = .5*(A+A');
+    
+    % Create the problem structure.
+    manifold = spherefactory(n);
+    problem.M = manifold;
+    
+    % Define the problem cost function
+    problem.cost  = @(x) -x'*(A*x);
+    
+    % Provide the gradient and the hessian via automatic differentiation
+    problem = preprocessAD(problem);
+    
+    % If the egrad has already been provided, the ehess will be computed 
+    % according to the egrad, which maybe faster based on the expression 
+    % of the egrad.
+    % problem.egrad = @(x) -2*(A*x);
+    % problem = preprocessAD(problem);
+
+    % If the user only wants the gradient or the hessian information,
+    % indicate the second argument of preprocessAD to be 'egrad' or 'ehess'
+
+    % e.g. provide the gradient only and use FN approximation of hessian
+    % (which is often faster than providing the exact hessian).
+    % problem = preprocessAD(problem,'egrad');
+
+    % Numerically check gradient and Hessian consistency.
+    figure;
+    checkgradient(problem);
+    figure;
+    checkhessian(problem);
+    
+    % Solve.
+    [x, xcost, info] = trustregions(problem);          %#ok<ASGLU>
+    
+    % Display some statistics.
+    figure;
+    semilogy([info.iter], [info.gradnorm], '.-');
+    xlabel('Iteration #');
+    ylabel('Gradient norm');
+    title('Convergence of the trust-regions algorithm on the sphere');
+    
+end
