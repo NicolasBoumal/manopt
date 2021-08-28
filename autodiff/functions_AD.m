fbecaf7e6f703990c74e2c99b7514bb01f552898
Xiaowen Jiang <71407346+Xiaowen-Jiang@users.noreply.github.com>
2021-08-28 19:50:43 +0200

    Delete autodiff directory

--- autodiff/functions_AD.m
+++
@@ -1,5 +0,0 @@
-%% TO do
-% try using preliminar functions in the folder /complexfunctions when 
-% customizing your cost function. An alternative way is to define one's 
-% own preliminary functions which should support both numerical arrays and 
-% structures with fields real and imag.
