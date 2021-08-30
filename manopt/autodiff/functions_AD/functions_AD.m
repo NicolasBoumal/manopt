539c7d2e6d053b43760ae96cd4d73743d6297ad0
Xiaowen Jiang <71407346+Xiaowen-Jiang@users.noreply.github.com>
2021-08-30 11:35:20 +0200

    Added some limitations

--- manopt/autodiff/functions_AD/functions_AD.m
+++ manopt/autodiff/functions_AD/functions_AD.m
@@ -7,36 +7,36 @@ function functions_AD()
 % The current implementation of AD is based on Matlab's deep learning
 % tool box and dlarray is the key data object which enables functions to  
 % compute derivatives via automatic differentiation. However, there is a
-% couple of functions which does not support dlarray operations. See the
+% couple of functions which do not support dlarray operations. See the
 % following official website: https://ch.mathworks.com/help/deeplearning/
 % ug/list-of-functions-with-dlarray-support.html. Also, operations
-% containing complex numbers between dlarrays are introduced in Matlab
-% R2021b. So AD fails when the user wants to optimize over complex 
-% manifolds when they cannot have access to Matlab R2021b or later. 
-% In these cases, manopt provides a list of backup functions which are 
-% implemented using the functions with AD support so that they have the  
-% same functionality but support automatic differentiation.
+% containing complex numbers between dlarrays are only introduced in Matlab
+% R2021b or later. So AD fails when the user wants to optimize over complex 
+% manifolds when the Matlab installed is R2021a or earlier. In these cases,
+% manopt provides a list of backup functions which are implemented using
+% the functions with AD support so that they have the same functionality
+% but support automatic differentiation.
 %
 % For Matlab R2021b or later: Complex numbers are supported. Users only
-% need to take care of the functions with or without AD support. Here is 
-% the list of common functions wihout AD support and they should be  
+% need to take care of those functions which do not support dlarray. Here 
+% is the list of common functions wihout AD support and they should be  
 % replaced by the corresponding backup functions when specifying the cost.
 % trace->ctrace               triu->ctriu                    tril->ctril     
 % norm(...,'fro') ->cnormfro              norm(...,'fro')^2 ->cnormsqfro 
 % diag->cdiag           multiscale->cmultiscale
 % All the other multi*** functions in manopt are supported.
-% bsxfunc is not supported. The user may have to translate it
-% into repmat and .* expressions. Concatenating arrays along the third 
-% dimension cat(A,B,3) is not supported. 
+% Moreover, bsxfunc is not supported. The user may have to translate it
+% into repmat and .* expressions. Concatenating arrays along the third or
+% higher dimension cat(A,B,3+) is not supported for AD.
 % Besides, the common functions: sqrtm, logm, expm, eig, svd, det, cumsum, 
 % movsum, cumprod, \, mod, rem, vecnorm, bandwidth are not supported. 
 %
 % For Matlab R2021a or earlier, in addition to the functions mentioned  
 % above, dot is not supported. The element-wise multiplication can be 
-% replaced by cdotprod. Moreover, to deal with complex problems, one way 
-% is to convert complex numbers into a structure with fields real and
-% imag. See mat2dl_complex and dl2mat_complex for more details. In this
-% case, the user should try using preliminary functions in the folder 
+% replaced by cdotprod. Moreover, to deal with complex problems, manopt 
+% converts complex numbers into a structure with fields real and imag.
+% See mat2dl_complex and dl2mat_complex for more details. In this case,
+% the user should try using the preliminary functions in the folder 
 % /functions_AD when customizing the cost function. An alternative way is 
 % to define one's own preliminary functions. These functions which should 
 % accept both numerical arrays and structures with fields real and imag. 
