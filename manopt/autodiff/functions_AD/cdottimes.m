74240fcbc19ef26792fe48d1c0da28711c9ad73b
Nicolas Boumal <nicolasboumal@gmail.com>
2021-09-05 12:05:24 +0200

    renames cdotprod into cdottimes

---
+++ manopt/autodiff/functions_AD/cdottimes.m
@@ -0,0 +1,47 @@
+function cdottimesAB = cdottimes(A, B)
+% Computes the element-wise multiplication between A and B
+%
+% function cdottimesAB = cdottimes(A, B)
+%
+% Returns the element-wise multiplication of A and B. The inputs A and B 
+% can be either numeric arrays or structs with fields real and imag.
+%
+% See also: manoptAD
+
+% This file is part of Manopt: www.manopt.org.
+% Original author: Xiaowen Jiang, July 31, 2021.
+% Contributors: Nicolas Boumal
+% Change log: 
+
+    if isnumeric(A) && isnumeric(B)
+        cdottimesAB = A .* B;
+        
+    elseif iscstruct(A) || iscstruct(B)
+        A = tocstruct(A);
+        B = tocstruct(B);
+        cdottimesAB.real = A.real .* B.real - A.imag .* B.imag;
+        cdottimesAB.imag = A.real .* B.imag + A.imag .* B.real;
+        
+    else
+        ME = MException('cprod:inputError', ...
+                        'Input does not have the expected format.');
+        throw(ME);
+        
+    end
+
+end
+
+function flag = iscstruct(A)
+    flag = ( isstruct(A) && isfield(A, 'real') && isfield(A, 'imag') );
+end
+
+function B = tocstruct(A)
+    if iscstruct(A)
+        B = A;
+    elseif isnumeric(A)
+        B.real = real(A);
+        B.imag = imag(A);
+    else
+        error('Input does not have the expected format.');
+    end
+end
