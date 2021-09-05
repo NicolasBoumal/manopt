2fe795dbebfddde377a1f2716c80478ba1e135ae
Nicolas Boumal <nicolasboumal@gmail.com>
2021-09-05 12:18:00 +0200

    cmultiscale now also works for cstruct scale

--- manopt/autodiff/functions_AD/cmultiscale.m
+++ manopt/autodiff/functions_AD/cmultiscale.m
@@ -1,10 +1,11 @@
 function Ascale = cmultiscale(scale, A)
-% Multiplies the 2D slices in a 3D matrix by individual scalars.
+% Multiplies the 2D slices in a 3D array by individual scalars.
 %
 % function Ascale = cmultiscale(scale, A)
 %
-% Basically the same as multiscale but is compatible with dlarrays
-% and structs with fields real and imag.
+% Basically the same as multiscale but is compatible with structs with
+% fields real and imag, which means this can be used for automatic
+% differentiation with complex arrays in older Matlab versions.
 %
 % See also: manoptAD multiscale
 
@@ -13,26 +14,56 @@ function Ascale = cmultiscale(scale, A)
 % Contributors: Nicolas Boumal
 % Change log: 
 
-    assert(ndims(A) == 3, ...
-           'cmultiscale is only well defined for matrix arrays of 3.');
-    if isstruct(A) && isfield(A,'real')
-        [~, ~, N] = size(A.real);
-        assert(numel(scale) == N, ...
-           ['scale must be a vector whose length equals the third ' ...
-            'dimension of A, that is, the number of 2D matrix slices ' ...
-            'in the 3D array A.']);
-        Ascale = cdotprod(A,reshape(scale,1,1,N));
-    elseif isnumeric(A)
-        [~, ~, N] = size(A);
-        assert(numel(scale) == N, ...
-           ['scale must be a vector whose length equals the third ' ...
-            'dimension of A, that is, the number of 2D matrix slices ' ...
-            'in the 3D array A.']);
-        Ascale = A.*reshape(scale,1,1,N);
+    if isnumeric(A) && isnumeric(scale)
+        
+        Ascale = multiscale(scale, A);
+        
     else
-        ME = MException('cmultiscale:inputError', ...
-        'Input does not have the expected format.');
-        throw(ME);
+        
+        A = tocstruct(A);
+        scale = tocstruct(scale);
+        
+        assert(ndims(A.real) == 3, ...
+           'cmultiscale is only well defined for 3D arrays.');
+       
+        [~, ~, N] = size(A.real);
+        
+        assert(numel(scale.real) == N, ...
+           ['scale must be a vector whose length equals the third ' ...
+            'dimension of A, that is, the number of 2D matrix slices ' ...
+            'in the 3D array A. It can also be a struct with fields ' ... 
+            'real and imag with size as stated above.']);
+        
+        scale.real = reshape(scale.real, 1, 1, N);
+        scale.imag = reshape(scale.imag, 1, 1, N);
+        Ascale = cdottimes(A, scale);
+
     end
 
 end
+
+
+% Test code
+% n = 3; m = 5; N = 17;
+% A = randn(n, m, N);
+% scale = randn(N, 1);
+% Z = multiscale(scale, A) - cmultiscale(scale, A);
+% norm(Z(:))
+% A = randn(n, m, N) + 1i*randn(n, m, N);
+% Z = multiscale(scale, A) - cmultiscale(scale, A);
+% norm(Z(:))
+% scale = randn(N, 1) + 1i*randn(N, 1);
+% Z = multiscale(scale, A) - cmultiscale(scale, A);
+% norm(Z(:))
+% B.real = real(A); B.imag = imag(A);
+% Z = cmultiscale(scale, B);
+% Zr = real(multiscale(scale, A)) - Z.real;
+% Zi = imag(multiscale(scale, A)) - Z.imag;
+% norm(Zr(:))
+% norm(Zi(:))
+% scalebis = tocstruct(scale);
+% Z = cmultiscale(scalebis, B);
+% Zr = real(multiscale(scale, A)) - Z.real;
+% Zi = imag(multiscale(scale, A)) - Z.imag;
+% norm(Zr(:))
+% norm(Zi(:))
