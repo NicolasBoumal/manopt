25ccef5b9284ed7d016ad782612fc5a6162aac86
Nicolas Boumal <nicolasboumal@gmail.com>
2021-09-05 16:20:55 +0200

    rewrote the help file

--- manopt/autodiff/manoptADhelp.m
+++ manopt/autodiff/manoptADhelp.m
@@ -1,53 +1,76 @@
 function manoptADhelp()
-% This file provides certain comments regarding the use of automatic
-% differentiation in manopt. It summarizes the limitations to define the  
-% cost function when the user wants to use automatic differentiation to 
-% provide the information of the gradient and the hessian. This file also 
-% provides a list of backup functions to address such issues.
+% Automatic differentation (AD) in Manopt requires the following:
+%   Matlab version R2021a or later.
+%   Deep Learning Toolbox version 14.2 or later.
 %
-% The current implementation of AD is based on Matlab's deep learning
-% tool box in which dlarray is the key data object which enables functions 
-% to compute derivatives via automatic differentiation. However, there is a
-% couple of functions which do not support dlarray operations. See the
-% following official website: 
-% https://ch.mathworks.com/help/deeplearning/ug/list-of-functions-with-dlarray-support.html. 
-% Also, operations containing complex numbers between dlarrays are only 
-% introduced in Matlab R2021b or later. So AD fails when the user wants to 
-% optimize over complex manifolds when the Matlab installed is R2021a or 
-% earlier. In these cases, manopt provides a list of backup functions which 
-% are implemented using the functions with AD support so that they have the 
-% same functionality but support automatic differentiation.
-% 
-% For Matlab R2021b or later: Complex numbers are supported. Users only
-% need to take care of those functions which do not support dlarray. Here 
-% is the list of common functions without AD support and they should be  
-% replaced by the corresponding backup functions when specifying the cost.
-% trace->ctrace
-% triu->ctriu
-% tril->ctril     
-% norm(...,'fro') ->cnormfro
-% norm(...,'fro')^2 ->cnormsqfro 
-% diag->cdiag
-% multiscale->cmultiscale
-% All the other multi*** functions in manopt are supported.
+% First, read the documentation of manoptAD by typing:
+%
+%   help manoptAD
+%
+% The comments here provide further information about how to use AD. These
+% comments are necessary because of certain limitations both of the AD
+% capabilities of the Deep Learning Toolbox and of Manopt's ability to
+% handle certain delicate geometries, e.g., fixed-rank matrices, with AD.
+%
+% The basic usage of AD in Manopt goes as follows:
+%
+%   problem.M = ...; %call a factory to get a manifold structure
+%   problem.cost = @(x) ...; % implement your cost function
+%   problem = manoptAD(problem); % ask AD to figure out gradient + Hessian
+%   ...; % call a solver on problem, e.g., x = trustregions(problem);
+%
+% The current implementation of AD is based on Matlab's Deep Learning
+% Toolbox. The latter relies on dlarray to represent data (arrays).
+% While this works very well, unfortunately, there are a few limitations.
+% For example, certain functions do not support dlarray.
+% See the following official website for a list of compatbile functions: 
+%
+%   https://ch.mathworks.com/help/deeplearning/ug/list-of-functions-with-dlarray-support.html.
+%
+% Moreover, dlarray only supports complex variables starting with
+% Matlab R2021b.
+%
+% To handle complex numbers in R2021a and earlier, and also to handle
+% functions that are not supported by dlarray at this moment, Manopt
+% provides a limited collection of backup functions which are compatible
+% with AD. Explicitly, the left column below lists commonly used functions
+% which are not supported by dlarray at this time, and the right column
+% lists corresponding replacement functions that can be used:
+%
+%     trace                 ctrace
+%     diag                  cdiag
+%     triu                  ctriu
+%     tril                  ctril
+%     norm(..., 'fro')      cnormfro
+%     norm(..., 'fro')^2    cnormsqfro
+%     multiscale            cmultiscale
+%
+% All the other multi*** functions in Manopt support AD.
+%
 % Moreover, bsxfun is not supported. The user may have to translate it
-% into repmat and point-wise expressions. Concatenating arrays along the 
-% third or higher dimension cat(A,B,3+) is not supported for AD.
-% Besides, the matrix functions sqrtm, logm, expm, eig, svd, det, cumsum, 
-% movsum, cumprod, \, inv, mod, rem, vecnorm, bandwidth are not supported. 
+% into repmat and point-wise expressions.
+% Concatenating arrays along the third or higher dimension such as in
+% cat(A, B, 3+) is not supported for AD.
+% The matrix functions sqrtm, logm, expm, eig, svd, det, cumsum,  movsum,
+% cumprod, \, inv, mod, rem, vecnorm, bandwidth are not supported.
 %
 % For Matlab R2021a or earlier, in addition to the functions mentioned  
-% above, dot is not supported. The element-wise multiplication can be 
-% replaced by cdottimes. Moreover, to deal with complex problems, manopt 
-% converts complex numbers into a structure with fields real and imag.
+% above, dot is not supported.
+% Element-wise multiplication can be replaced by cdottimes.
+%
+% To deal with complex variables in R2021a and earlier, Manopt converts
+% complex arrays into a structure with fields real and imag.
 % See mat2dl_complex and dl2mat_complex for more details. In this case,
 % the user should try using the basic functions in the folder 
-% /functions_AD when defining the cost function. An alternative way is 
-% to define one's own basic functions. These functions should 
-% accept both numeric arrays and structures with fields real and imag. 
+%
+%   /manopt/autodiff/functions_AD
+%
+% when defining the cost function. An alternative way is to define one's
+% own basic functions. These functions should accept both numeric arrays
+% and structures with fields real and imag.
 % See cprod and complex_example_AD as examples.
 %
-% See also: manoptAD, complex_example_AD
+% See also: manoptAD complex_example_AD
 
 % This file is part of Manopt: www.manopt.org.
 % Original author: Xiaowen Jiang, Aug. 31, 2021.
