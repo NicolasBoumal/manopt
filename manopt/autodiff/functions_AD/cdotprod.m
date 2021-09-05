e5d3438943f42c8fcf06f281aefc38b11ae625a6
Nicolas Boumal <nicolasboumal@gmail.com>
2021-09-05 11:50:42 +0200

    refactored cdotprof

--- manopt/autodiff/functions_AD/cdotprod.m
+++ manopt/autodiff/functions_AD/cdotprod.m
@@ -1,42 +1,47 @@
-function cdotprodAB = cdotprod(A,B)
+function cdotprodAB = cdotprod(A, B)
 % Computes the element-wise multiplication between A and B
 %
-% function cdotprodAB = cdotprod(A,B)
+% function cdotprodAB = cdotprod(A, B)
 %
-% Returns the element-wise multiplication of A and B. The input A and B 
-% can be either a numeric array or a struct with fields real and imag.
+% Returns the element-wise multiplication of A and B. The inputs A and B 
+% can be either numeric arrays or structs with fields real and imag.
 %
 % See also: manoptAD
 
 % This file is part of Manopt: www.manopt.org.
-% Original author: Xiaowen Jiang, July. 31, 2021.
+% Original author: Xiaowen Jiang, July 31, 2021.
 % Contributors: Nicolas Boumal
 % Change log: 
 
-    if isnumeric(A) && isstruct(B) && isfield(B,'real')
-        realA = real(A);
-        imagA = imag(A);
-        cdotprodAB.real = realA.*B.real-imagA.*B.imag;
-        cdotprodAB.imag = realA.*B.imag+imagA.*B.real;
+    if isnumeric(A) && isnumeric(B)
+        cdotprodAB = A .* B;
         
-    elseif isstruct(A) && isnumeric(B) && isfield(A,'real')
-        realB = real(B);
-        imagB = imag(B);
-        cdotprodAB.real = A.real.*realB-A.imag.*imagB;
-        cdotprodAB.imag = A.real.*imagB+A.imag.*realB;
-        
-    elseif isnumeric(A) && isnumeric(B)
-        cdotprodAB = A.*B;
-        
-    elseif isstruct(A) && isstruct(B) && isfield(A,'real') && isfield(B,'real')
-        cdotprodAB.real = A.real.*B.real-A.imag.*B.imag;
-        cdotprodAB.imag = A.real.*B.imag+A.imag.*B.real;
+    elseif iscstruct(A) || iscstruct(B)
+        A = tocstruct(A);
+        B = tocstruct(B);
+        cdotprodAB.real = A.real .* B.real - A.imag .* B.imag;
+        cdotprodAB.imag = A.real .* B.imag + A.imag .* B.real;
         
     else
         ME = MException('cprod:inputError', ...
-        'Input does not have the expected format.');
+                        'Input does not have the expected format.');
         throw(ME);
         
     end
 
+end
+
+function flag = iscstruct(A)
+    flag = ( isstruct(A) && isfield(A, 'real') && isfield(A, 'imag') );
+end
+
+function B = tocstruct(A)
+    if iscstruct(A)
+        B = A;
+    elseif isnumeric(A)
+        B.real = real(A);
+        B.imag = imag(A);
+    else
+        error('Input does not have the expected format.');
+    end
 end
