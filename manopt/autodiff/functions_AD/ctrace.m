0aca2965a2f79bb4aaecce891703327910f1212f
Nicolas Boumal <nicolasboumal@gmail.com>
2021-09-05 12:26:44 +0200

    refactored + documentation

--- manopt/autodiff/functions_AD/ctrace.m
+++ manopt/autodiff/functions_AD/ctrace.m
@@ -3,22 +3,23 @@ function traceA = ctrace(A)
 %
 % function traceA = ctrace(A)
 %
-% Returns the sum of diagonal elements of A. The input A does not
-% necessarily to be a square matrix. The function supports both numeric 
-% arrays and structs with fields real and imag. Note that trace currently
-% does not support dlarrays and ctrace can be seen as a backup function.
+% Returns the sum of diagonal elements of A. The input A does not have
+% to be a square matrix. The function supports both numeric  arrays and
+% structs with fields real and imag. This file was created because trace
+% is not currently supported by dlarrays which we use for automatic
+% differentiation: ctrace is a backup function.
 %
 % See also: manoptAD
 
 % This file is part of Manopt: www.manopt.org.
-% Original author: Xiaowen Jiang, July. 31, 2021.
+% Original author: Xiaowen Jiang, July 31, 2021.
 % Contributors: Nicolas Boumal
 % Change log:
 
-    if isstruct(A) && isfield(A,'real')
-        assert(length(size(A.real))==2,'Input should be a 2-D array')
-        m = size(A.real,1);
-        n = size(A.real,2);
+    if iscstruct(A)
+        assert(length(size(A.real)) == 2, 'Input should be a 2-D array')
+        m = size(A.real, 1);
+        n = size(A.real, 2);
         realA = A.real;
         imagA = A.imag;
         
@@ -31,7 +32,7 @@ function traceA = ctrace(A)
         end
         
     elseif isnumeric(A)
-        assert(length(size(A))==2,'Input should be a 2-D array')
+        assert(length(size(A)) == 2, 'Input should be a 2-D array')
         m = size(A,1);
         n = size(A,2);
         if n >= m
@@ -44,6 +45,7 @@ function traceA = ctrace(A)
         ME = MException('ctrace:inputError', ...
         'Input does not have the expected format.');
         throw(ME);
+        
     end
 
 end
