5b1b41edd0458cf1a0eaca5c5dc0f46d871e3738
Victor <victorliao18@gmail.com>
2022-07-04 01:32:17 +0200

    use problem.linesearch and getlinesearch for constant stepsize

--- manopt/solvers/linesearch/linesearch_constant.m
+++ manopt/solvers/linesearch/linesearch_constant.m
@@ -1,10 +1,12 @@
 function [stepsize, newx, newkey, lsstats] = ...
-                  stepsize_constant(problem, x, d, ~, ~, options, storedb, ~)
+                  linesearch_constant(problem, x, d, ~, ~, ~, storedb, ~)
 % Constant stepsize algorithm for descent methods
-%
-% function [stepsize, newx, newkey, lsstats] = 
-%            stepsize_constant(problem, x, d, ~, ~, options, storedb, ~)
 % 
+% Note: user should define their intended alpha and specify 
+% linesearch_constant as follows:
+%  problem.linesearch = @(x, d) alpha;
+%  options.linesearch = @linesearch_constant;
+%
 % Below, the step is constructed as alpha*d, and the step size is the norm
 % of that vector, thus: stepsize = alpha*norm_d. The step is executed by
 % retracting the vector alpha*d from the current point x, giving newx.
@@ -14,14 +16,9 @@ function [stepsize, newx, newkey, lsstats] = ...
 %  problem : structure holding the description of the optimization problem
 %  x : current point on the manifold problem.M
 %  d : tangent vector at x (descent direction)
-%  f0 : cost value at x
-%  df0 : directional derivative at x along d
-%  options : options structure (see in code for usage):
-%     stepsize_init (0.01)
-%         The constant stepsize alpha that is used at every iteration.
 %  storedb : StoreDB object (handle class: passed by reference) for caching
 %
-%  options and storedb are optional.
+%  storedb is optional.
 %
 % Outputs
 %
@@ -39,26 +36,25 @@ function [stepsize, newx, newkey, lsstats] = ...
 % Contributors: 
 % Change log: 
 
-    % Allow omission of storedb.
-    if ~exist('storedb', 'var')
-        storedb = StoreDB();
+    % Allow omission of the key, and even of storedb.
+    if ~exist('key', 'var')
+        if ~exist('storedb', 'var')
+            storedb = StoreDB();
+        end
     end
 
-    % Constant stepsize default parameters. These can be overwritten in the
-    % options structure which is passed to the solver.
-    default_options.stepsize_init = 0.01;
-
-    if ~exist('options', 'var') || isempty(options)
-        options = struct();
+    % Obtain user-specified alpha.
+    % User should specify their intended "stepsize" by:
+    % problem.linesearch = @(x,d) "stepsize";
+    if canGetLinesearch(problem)
+        alpha = getLinesearch(problem, x, d);
+    else
+        alpha = 1; % default alpha value.
     end
-    options = mergeOptions(default_options, options);
-    
-    alpha = options.stepsize_init;
 
     % Make the chosen step and compute the cost there.
     newx = problem.M.retr(x, d, alpha);
     newkey = storedb.getNewKey();
-    cost_evaluations = 1;
     
     % As seen outside this function, stepsize is the size of the vector we
     % retract to make the step from x to newx. Since the step is alpha*d:
@@ -66,7 +62,6 @@ function [stepsize, newx, newkey, lsstats] = ...
     stepsize = alpha * norm_d;
     
     % Return some statistics also, for possible analysis.
-    lsstats.costevals = cost_evaluations;
     lsstats.stepsize = stepsize;
     lsstats.alpha = alpha;
 end
--- manopt/solvers/steepestdescent/steepestdescent.m
+++ manopt/solvers/steepestdescent/steepestdescent.m
@@ -41,16 +41,17 @@ function [x, cost, info, options] = steepestdescent(problem, x, options)
 %   minstepsize (1e-10)
 %       The algorithm terminates if the linesearch returns a displacement
 %       vector (to be retracted) smaller in norm than this value.
-%   linesearch (@linesearch or @linesearch_hint or @stepsize_constant)
+%   linesearch (@linesearch or @linesearch_hint or @linesearch_constant)
 %       Function handle to a line search function. The options structure is
 %       passed to the line search too, so you can pass it parameters. See
 %       each line search's documentation for info. Another available line
 %       search in manopt is @linesearch_adaptive, in
 %       /manopt/linesearch/linesearch_adaptive.m
 %       If the problem structure includes a line search hint, then the
-%       default line search used is @linesearch_hint. 
-%       If @stepsize_constant is used the default constant stepsize is 0.01
-%       but it can be specified by options.stepsize_init
+%       default line search used is @linesearch_hint.
+%       If @linesearch_constant is used the default constant "stepsize" is 
+%       1 but this can be specified by:
+%           problem.linesearch = @(x,d) "stepsize";
 %   statsfun (none)
 %       Function handle to a function that will be called after each
 %       iteration to provide the opportunity to log additional statistics.
