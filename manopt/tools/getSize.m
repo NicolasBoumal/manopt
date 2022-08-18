bdbf4a31f29ae0f1df151e60bfddeb450d14960d
Victor <victorliao18@gmail.com>
2022-08-18 14:28:37 +0200

    add more documentation for tangentorthobasis and restructure getSize code

--- manopt/tools/getSize.m
+++ manopt/tools/getSize.m
@@ -1,6 +1,5 @@
-function [ bytes ] = getSize( variable )
-
-% Method to get the bytes of a struct.
+function bytes = getsize(variable)
+% Method to get the bytes of an object.
 %
 % Source:
 % https://ch.mathworks.com/matlabcentral/answers/14837-how-to-get-size-of-an-object
@@ -9,21 +8,26 @@ function [ bytes ] = getSize( variable )
 
 % This file is part of Manopt: www.manopt.org.
 % Original authors: Dmitry Borovoy and Mario Reutter
-% Contributors: 
-% Change log: 
+% Contributors: Victor Liao
+% Change log:
+%
+%   VL July 25, 2022:
+%       Improve code readability and structure.
 
-props = properties(variable); 
-if size(props, 1) < 1, bytes = whos(varname(variable)); bytes = bytes.bytes;
-else % code of Dmitry
-  bytes = 0;
-  for ii=1:length(props)
-      currentProperty = getfield(variable, char(props(ii)));
-      s = whos(varname(currentProperty));
-      bytes = bytes + s.bytes;
-  end
-end
+    props = properties(variable); 
+    if size(props, 1) < 1
+        bytes = whos(varname(variable));
+        bytes = bytes.bytes;
+    else % code of Dmitry
+      bytes = 0;
+      for ii = 1:length(props)
+          currentProperty = getfield(variable, char(props(ii)));
+          s = whos(varname(currentProperty));
+          bytes = bytes + s.bytes;
+      end
+    end
 end
 
-function [ name ] = varname( ~ )
-name = inputname(1);
+function name = varname( ~ )
+    name = inputname(1);
 end
