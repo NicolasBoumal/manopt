af224b175f0cd66a64c78f08f6856948281d5303
Victor <victorliao18@gmail.com>
2022-08-18 18:28:31 +0200

    add exact subproblem solver trs_gep which is a wrapper for Nakatsukasa's code in TRSgep which is also modified accordingly

--- manopt/solvers/trustregions/TRSgep.m
+++ manopt/solvers/trustregions/TRSgep.m
@@ -1,34 +1,54 @@
-function [x,lam1] = TRSgep(A,a,B,Del)
-% Solves the trust-region subproblem by a generalized eigenproblem without
-% iterations
+
+
+function [x, limitedbyTR] = TRSgep(A, a, Del)
+% Solves trust-region subproblem by a generalized eigenvalue problem.
 % 
 % minimize (x^TAx)/2+ ax
-% subject to x^TBx <= Del^2
+% subject to x^Tx <= Del^2
 %
 % A: nxn symmetric, a: nx1 vector
 % B: nxn symmetric positive definite
-% 
-% Yuji Nakatsukasa, 2015
-% Revised by Nikitas Rontsis, December 2018
+%
+% function [x, limitedbyTR] = TRSgep(A, a, Del)
+%
+% Code adapted from Yuji Nakatsukasa's code from the
+% paper by Satoru Adachi, Satoru Iwata, Yuji Nakatsukasa, and Akiko Takeda
+%
+% Code adapted from: https://people.maths.ox.ac.uk/nakatsukasa/codes/TRSgep.m
+% Paper code is based on: https://epubs.siam.org/doi/abs/10.1137/16M1058200
+%
+% The authors kindly allowed us to include their code in Manopt under the 
+% same license as Manopt.
+%
+% See also: trs_gep trs_tCG_cached trs_tCG trustregions
+
+% This file is part of Manopt: www.manopt.org.
+% Original author: Yuji Nakatsukasa, 2015.
+% Contributors: Revised by Nikitas Rontsis, December 2018
+% Change log:
+%   VL June 29, 2022:
+%       Modified original code to return limitedbyTR boolean and change
+%       ellipsoid norm constraint to unweighted norm.
 
 n = size(A,1);
-if issparse(B)
-MM1 = [sparse(n,n) B;B sparse(n,n)];
-else
-MM1 = [zeros(n) B;B zeros(n)];    
-end
+
+% returned boolean to trustregions.m. true if we are limited by the TR
+% boundary (returns boundary solution). Otherwise false.
+limitedbyTR = false;
+
+MM1 = [sparse(n,n) speye(n);speye(n) sparse(n,n)];
 tolhardcase = 1e-4; % tolerance for hard-case
 
-p1 = pcg(A,-a,1e-12, 500); % possible interior solution
+[p1, ~] = pcg(A,-a,1e-12, 500); % possible interior solution
 if norm(A*p1+a)/norm(a)<1e-5,
-if p1'*B*p1>=Del^2, p1 = nan;
+if p1'*p1>=Del^2, p1 = nan;
 end
 else
     p1 = nan;
     end
 
 % This is the core of the code
-[V,lam1] = eigs(@(x)MM0timesx(A,B,a,Del,x),2*n,-MM1,1,'lr'); 
+[V,lam1] = eigs(@(x)MM0timesx(A,a,Del,x),2*n,-MM1,1,'lr'); 
 
     if norm(real(V)) < 1e-3 %sometimes complex
         V = imag(V);    else        V = real(V);
@@ -36,7 +56,7 @@ else
 
     lam1 = real(lam1);
     x = V(1:length(A)); % this is parallel to soln
-    normx = sqrt(x'*(B*x));         
+    normx = sqrt(x'*(x));         
     x = x/normx*Del; % in the easy case, this naive normalization improves accuracy
     if x'*a>0, x = -x; end % take correct sign
     
@@ -45,16 +65,16 @@ if normx < tolhardcase % hard case
 x1 = V(length(A)+1:end);
 alpha1 = lam1;
 Pvect = x1;  %first try only k=1, almost always enough
-x2 = pcg(@(x)pcgforAtilde(A,B,lam1,Pvect,alpha1,x),-a,1e-12,500);
-if norm((A+lam1*B)*x2+a)/norm(a)>tolhardcase, % large residual, repeat
+[x2, ~] = pcg(@(x)pcgforAtilde(A,lam1,Pvect,alpha1,x),-a,1e-12,500);
+if norm((A+lam1)*x2+a)/norm(a)>tolhardcase, % large residual, repeat
     for ii = 3*[1:3]
-    [Pvect,DD] = eigs(A,B,ii,'sa');
-    x2 = pcg(@(x)pcgforAtilde(A,B,lam1,Pvect,alpha1,x),-a,1e-8,500);    
-    if norm((A+lam1*B)*x2+a)/norm(a) < tolhardcase, break, end
+    [Pvect,DD] = eigs(A,speye(n),ii,'sa');
+    [x2, ~] = pcg(@(x)pcgforAtilde(A,lam1,Pvect,alpha1,x),-a,1e-8,500);    
+    if norm((A+lam1)*x2+a)/norm(a) < tolhardcase, break, end
     end
 end
 
-Bx = B*x1; Bx2 = B*x2; aa = x1'*(Bx); bb = 2*x2'*Bx; cc = (x2'*Bx2-Del^2); 
+aa = x1'*(x1); bb = 2*x2'*x1; cc = (x2'*x2-Del^2); 
 alp = (-bb+sqrt(bb^2-4*aa*cc))/(2*aa); %norm(x2+alp*x)-Delta
 x = x2+alp*x1;
 end
@@ -64,28 +84,31 @@ end
 if sum(isnan(p1))==0,
 if (p1'*A*p1)/2+a'*p1 < (x'*A*x)/2+a'*x, 
     x = p1; lam1 = 0;
+    
+    % specify we have a boundary solution
+    limitedbyTR = true;
 end
 end
 end
 
 
 
-function [y] = MM0timesx(A,B,g,Delta,x)
-% MM0 = [-B A;
+function [y] = MM0timesx(A,g,Delta,x)
+% MM0 = [-Id A;
 %         A -g*g'/Delta^2];
 n = size(A,1); 
 x1 = x(1:n); x2 = x(n+1:end);
-y1 = -B*x1 + A*x2;
+y1 = -x1 + A*x2;
 y2 = A*x1-g*(g'*x2)/Delta^2;
 y = [y1;y2];
 end
 
-function [y] = pcgforAtilde(A,B,lamA,Pvect,alpha1,x)
+function [y] = pcgforAtilde(A,lamA,Pvect,alpha1,x)
 
 [n,m] = size(Pvect);
-y = A*x+lamA*(B*x);
+y = A*x+lamA*(x);
 
 for i=1:m
-    y = y+(alpha1*(x'*(B*Pvect(:,i))))*(B*Pvect(:,i));
+    y = y+(alpha1*(x'*(Pvect(:,i))))*(Pvect(:,i));
 end
 end
---
+++ manopt/solvers/trustregions/trs_gep.m
@@ -0,0 +1,151 @@
+function [eta, Heta, print_str, stats] = trs_gep(problem, subprobleminput, options, ~, ~)
+% Solves trust-region subproblem with TRSgep in a subspace of tangent space.
+%
+% minimize <eta,grad> + .5*<eta,Hess(eta)>
+% subject to <eta,eta> <= Delta^2
+%
+% function [eta, Heta, print_str, stats] = trs_gep(problem, subprobleminput, options, ~, ~)
+%
+% This is meant to be used by the trustregion solver.
+% To use this method, specify trs_gep as an option, and your chosen
+% subspace dimension in the problem structure, as follows:
+%
+% n = problem.M.dim()
+% options.subproblemsolver = @trs_gep;
+% options.gepsubspacedim = n; % any integer between 1 and n inclusive.
+% x = trustregions(problem, [], options)
+%
+% Note: trs_gep does not use preconditioning.
+%
+% trs_gep solves the trust-region subproblem exactly in a 
+% options.gepsubspacedim dimensional subspace of the tangent space by 
+% creating an orthonormal basis for the subspace using tangentorthobasis, 
+% and passing the hessian and gradient in that basis to TRSgep.
+% 
+% The basis is constructed by tangentorthobasis with randomly sampled 
+% linearly independent tangent vectors then orthonormalized with 
+% Gram-Schmidt.
+%
+% If options.gepsubspacedim < problem.M.dim() then when grad is nonzero, 
+% to guarantee that we do at least as well as the 
+% Cauchy point, we ensure grad is in the span of the basis.
+%
+% Inputs:
+%   problem: Manopt optimization problem structure
+%   subprobleminput: struct storing information for this subproblemsolver
+%       x: point on the manifold problem.M
+%       grad: gradient of the cost function of the problem at x
+%       Delta = trust-region radius
+%   options: structure containing options for the subproblem solver
+%   storedb, key: caching data for problem at x
+%
+% Options specific to this subproblem solver:
+%   gepsubspacedim (problem.M.dim())
+%       A value between 1 and problem.M.dim() inclusive that specifies the
+%       subspace dimension we wish to solve the trust-region subproblem
+%       over.
+%       
+% Outputs:
+%   eta: approximate solution to the trust-region subproblem at x
+%   Heta: Hess f(x)[eta] -- this is necessary in the outer loop, and it
+%       is often naturally available to the subproblem solver at the
+%       end of execution, so that it may be cheaper to return it here.
+%   print_str: subproblem specific string to be printed by trustregions.m
+%   stats: structure with values to be stored in trustregions.m
+%       hessvecevals: number of Hessian calls during execution (here we set
+%       it to gepsubspacedim since we construct the Hessian
+%       explicitly in gepsubspacedim dimensions.
+%       limitedbyTR: true if a boundary solution is returned
+%
+% trs_gep can also be called in the following way (for printing
+% purposes):
+%
+% function [~, ~, print_str, stats] = trs_gep([], [], options)
+%
+% In this case when nargin == 3, the returned stats struct contains the 
+% relevant fields along with their corresponding initial values. In this
+% case print_str is the header to be printed before the first pass of 
+% trustregions.m. The other outputs will be 
+% empty. This stats struct is used in the first call to savestats in 
+% trustregions.m to initialize the info struct properly.
+%
+% See also: TRSgep trs_tCG trustregions
+
+% This file is part of Manopt: www.manopt.org.
+% Original author: Victor Liao, June 13, 2022.
+% Contributors: 
+% Change log: 
+
+    if nargin == 3
+        % trustregions.m only wants default values for stats.
+        eta = [];
+        Heta = [];
+        print_str = sprintf('%9s   %s', 'hessvec','stopreason');
+        stats = struct('hessvecevals', 0, 'limitedbyTR', false);
+        return;
+    end
+
+    x = subprobleminput.x;
+    Delta = subprobleminput.Delta;
+    grad = subprobleminput.fgradx;
+    
+    if isfield(options, 'useRand') && options.useRand	
+        warning('manopt:trs_gep:useRand', ...
+        ['(options.useRand == true) but trs_gep does not use this option. It will be ignored.' ...
+        'To disable this warning: warning(''off'', ''manopt:trs_gep:useRand'')']);
+    end
+    
+    M = problem.M;
+
+    % Set local defaults here    
+    localdefaults.gepsubspacedim = M.dim();
+
+    % Merge global and local defaults, then merge w/ user options, if any.
+    localdefaults = mergeOptions(getGlobalDefaults(), localdefaults);
+    if ~exist('options', 'var') || isempty(options)
+        options = struct();
+    end
+    options = mergeOptions(localdefaults, options);
+    
+    % dimension of subspace we want to solve TRS over.
+    n = options.gepsubspacedim;
+    
+    assert(n >= 1 && n <= M.dim() && n == round(n), ...
+           'options.gepsubspacedim must be an integer between 1 and M.dim().');
+
+    % If gradient is nonzero, then even if n < M.dim() we
+    % guarantee to do at least as well as the cauchy point.
+    grad_norm = M.norm(x, grad);
+    if ~(grad_norm == 0)
+        basis_vecs = cell(1, 1);
+        basis_vecs{1} = grad;
+
+        B = tangentorthobasis(M, x, n, basis_vecs);
+
+        grad_vec = zeros(n, 1);
+        grad_vec(1) = grad_norm;
+    else
+        B = tangentorthobasis(M, x, n);
+
+        grad_vec = tangent2vec(M, x, B, grad);
+    end
+    
+    H = hessianmatrix(problem, x, B);
+
+    [eta_vec, limitedbyTR] = TRSgep(H, grad_vec, Delta);
+
+    eta = lincomb(M, x, B, eta_vec);
+    
+    % Instead of the below two lines one can simply use: Heta =
+    % getHessian(problem, x, eta, storedb, key). This will be problem
+    % dependent and can be reconsidered depending on application.
+    Heta_vec = H*eta_vec;
+    Heta = lincomb(M, x, B, Heta_vec);
+
+    if limitedbyTR
+        stopreason_str = 'Exact trs_gep boundary sol';
+    else
+        stopreason_str = 'Exact trs_gep interior sol';
+    end
+    print_str = sprintf('%9d   %s', n, stopreason_str);
+    stats = struct('hessvecevals', n, 'limitedbyTR', limitedbyTR);
