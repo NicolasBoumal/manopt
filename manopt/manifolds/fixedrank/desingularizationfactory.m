81761a41ea3bc6b858b7bbbfed2c79025e11fc32
Nicolas Boumal <nicolasboumal@gmail.com>
2024-06-04 16:20:05 +0200

    fixed M.vec

--- manopt/manifolds/fixedrank/desingularizationfactory.m
+++ manopt/manifolds/fixedrank/desingularizationfactory.m
@@ -247,7 +247,7 @@ function M = desingularizationfactory(m, n, r, alpha)
     M.vec = @vec;
     function Zvec = vec(X, Xd)
         VpS = Xd.Vp*sqrt(sfactor(X));
-        Zvec = [Xd(:); VpS(:)];
+        Zvec = [Xd.K(:); VpS(:)];  % TODO: changed Xd to Xd.K : ok?
     end
 
     % The function 'mat' is the left-inverse of 'vec'. It is sometimes
