f4eb59986cdd88a4db14e8cb135fd449fd45d328
Nicolas Boumal <nicolasboumal@gmail.com>
2024-06-26 17:05:57 +0200

    Update test_burermonteiro.m

--- tests/lifts/test_burermonteiro.m
+++ tests/lifts/test_burermonteiro.m
@@ -1,31 +1,19 @@
 clear; clf; clc;
 
-n = 10000;
+n = 1000;
 p = 15;
 
-constraint = 'unitdiag';
-lift = burermonteirolift(constraint, n, p, 'symmetric');
-Rnn = lift.N;
+C = abs(sprandsym(n, .001));
+
+Rnn = euclideanlargefactory(n, n);
 downstairs.M = Rnn;
-
-% sign = +1;
-% sqfrobnorm = @(Z) Z(:)'*Z(:);
-% downstairs.cost = @(X) sign*.5*Rnn.norm(X)^2;
-% downstairs.grad = @(X) Rnn.scale(sign, X);
-% downstairs.hess = @(X, Xdot) Rnn.scale(sign, Xdot);
-
-C = sparse(double(randsym(n) < -2));
-C(1:(n+1):end) = 0;
 downstairs.cost = @(X) Rnn.inner(C, X);
 downstairs.grad = @(X) C;
 downstairs.hess = @(X, Xdot) Rnn.zero();
 
-[upstairs, downstairs] = manoptlift(downstairs, lift);
+lift = burermonteirolift('unitdiag', n, p, 'symmetric');
 
-% checkgradient(downstairs);
-% checkhessian(downstairs);
-% checkgradient(upstairs);
-% checkhessian(upstairs);
+upstairs = manoptlift(downstairs, lift);
 
 Y0 = upstairs.M.rand();
 
@@ -35,12 +23,12 @@ manual.cost = @(Y) inner(C*Y, Y);
 manual.egrad = @(Y) 2*C*Y;
 manual.ehess = @(Y, Ydot) 2*C*Ydot;
 
-profile clear; pause;
-profile on;
+% profile clear;
+% profile on;
 [~, ~, infomanual] = trustregions(manual, Y0);
 [Y, ~, info] = trustregions(upstairs, Y0);
-profile off;
-profile report;
+% profile off;
+% profile report;
 
 semilogy([info.time], [info.gradnorm], '.-', ...
          [infomanual.time], [infomanual.gradnorm], '.-');
