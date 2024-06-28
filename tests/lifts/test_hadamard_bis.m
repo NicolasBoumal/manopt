3c226dda4a2c4880b3d6de48eec746dc4a66d7b0
Nicolas Boumal <nicolasboumal@gmail.com>
2024-06-28 14:34:31 +0200

    added comparison with cg

--- tests/lifts/test_hadamard_bis.m
+++ tests/lifts/test_hadamard_bis.m
@@ -28,26 +28,34 @@ downstairs.hess = @(x, xdot) A*xdot;
 
 upstairs = manoptlift(downstairs, lift);
 
-t_manopt = tic();
-[y, f_manopt, info] = trustregions(upstairs);
-x_manopt = lift.phi(y);
-t_manopt = toc(t_manopt);
+t_manopt_tr = tic();
+[y_tr, f_manopt_tr, info_tr] = trustregions(upstairs);
+x_manopt_tr = lift.phi(y_tr);
+t_manopt_tr = toc(t_manopt_tr);
+
+t_manopt_cg = tic();
+[y_cg, f_manopt_cg, info_cg] = conjugategradient(upstairs);
+x_manopt_cg = lift.phi(y_cg);
+t_manopt_cg = toc(t_manopt_cg);
 
 t_quadprog = tic();
 x_quadprog = quadprog(A, b, [], [], [], [], zeros(n, 1), []);
 t_quadprog = toc(t_quadprog);
 f_quadprog = getCost(downstairs, x_quadprog);
 
-fprintf('Manopt:  \n  Time: %.3e,  min(x): %.3e,  f(x): %.10e\n', ...
-            t_manopt, min(x_manopt), f_manopt);
-fprintf('Quadprog:\n  Time: %.3e,  min(x): %.3e,  f(x): %.10e\n', ...
+fprintf('Manopt TR:\n  Time: %.3e,  min(x): %.3e,  f(x): %.10e\n', ...
+            t_manopt_tr, min(x_manopt_tr), f_manopt_tr);
+fprintf('Manopt CG:\n  Time: %.3e,  min(x): %.3e,  f(x): %.10e\n', ...
+            t_manopt_cg, min(x_manopt_cg), f_manopt_cg);
+fprintf('Quadprog: \n  Time: %.3e,  min(x): %.3e,  f(x): %.10e\n', ...
             t_quadprog, min(x_quadprog), f_quadprog);
 
-reference = min([f_quadprog, f_manopt]);
-semilogy([info.time], [info.cost] - reference, '.-', ...
+reference = min([f_quadprog, f_manopt_tr, f_manopt_cg]);
+semilogy([info_tr.time], [info_tr.cost] - reference, '.-', ...
+         [info_cg.time], [info_cg.cost] - reference, '.-', ...
          t_quadprog, f_quadprog - reference, '.', ...
-         'LineWidth', 2, 'MarkerSize', 15);
+         'LineWidth', 2, 'MarkerSize', 25);
 grid on;
 xlabel('Computation time [s]');
 title('Difference between cost function value and best cost reached overall.');
-legend('Manopt', 'quadprog');
+legend('Manopt TR', 'Manopt CG', 'quadprog');
