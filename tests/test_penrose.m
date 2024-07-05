abca9e39eacb7ef2c3aadf6d4163aacec9159f9f
Nicolas Boumal <nicolasboumal@gmail.com>
2024-07-05 16:13:09 +0200

    refined comments

--- tests/test_penrose.m
+++ tests/test_penrose.m
@@ -11,12 +11,14 @@ problem.cost = @(X) .5*norm(A*X*B - C, 'fro')^2; % f(X)
 problem.egrad = @(X) A'*(A*X*B - C)*B';          % grad f(X) in R^(nxn)
 
 % Wrong.
-% This code treats Xdot as if it were in the embedding space.
+% This code treats the second input of ehess as if it were in
+% the embedding space. Optimization algorithms won't be amused.
 problem.ehess = @(X, Xdot) A'*(A*Xdot*B)*B';
 
 % Correct.
-% This code transforms Omg to X*Omg, which is the direction that is actually
-% encoded by Omg, and which we usually refer to as Xdot.
+% This code notes that the second input to ehess is a representation of
+% a tangent vector. It transforms Omg to X*Omg, which is the direction
+% that is actually encoded by Omg, and which we often call Xdot.
 % Equivalently, we can call SOn.tangent2ambient(X, Omg) instead of X*Omg.
 problem.ehess = @(X, Omg) A'*(A*X*Omg*B)*B';
 
