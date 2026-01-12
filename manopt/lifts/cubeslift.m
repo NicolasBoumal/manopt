93f770de3f705021a9bcc3d126b268fd5c1ce809
Nicolas Boumal <nicolasboumal@gmail.com>
2026-01-12 10:12:34 +0100

    Now allows to optimize in [-scale, scale]^(n x m), generalizing from [-1, 1]^(n x m)

--- manopt/lifts/cubeslift.m
+++ manopt/lifts/cubeslift.m
@@ -1,21 +1,25 @@
-function lift = cubeslift(n, m)
-% Sine lift y -> sin(y) for optimization in a box [-1, 1]^n with Manopt.
+function lift = cubeslift(n, m, scale)
+% Sine lift: y -> sin(y) for optimization in a box [-1, 1]^n with Manopt.
 %
 % function lift = cubeslift(n)
 % function lift = cubeslift(n, m)
+% function lift = cubeslift(n, m, scale)
 %
 % This function produces a lift structure to be used with manoptlift.
 %
 % If m is omitted, then m = 1 by default.
+% If scale is omitted, then scale = 1 by default.
 %
 % Both the upstairs manifold M and the downstairs manifold N consist of
 % real matrices of size n x m, that is,
 % 
 %   lift.M = lift.N = euclideanfactory(n, m).
 %
-% The lift is phi(y) = sin(y).
+% The lift is phi(y) = scale*sin(y).
 %
-% The image of the lift downstairs is phi(M) = [-1, 1]^(n x m): a box.
+% The image of the lift downstairs is a box:
+% 
+%   phi(M) = [-scale, scale]^(n x m).
 %
 % See also: manoptlift
 
@@ -23,18 +27,23 @@ function lift = cubeslift(n, m)
 % Original author: Nicolas Boumal, June 20, 2024.
 % Contributors: 
 % Change log: 
+%   Jan. 12, 2026 (NB): added 'scale' as an input.
 
     if ~exist('m', 'var') || isempty(m)
         m = 1;
+    end
+
+    if ~exist('scale', 'var') || isempty(scale)
+        scale = 1;
     end
 
     lift.M = euclideanfactory(n, m);
     lift.N = euclideanfactory(n, m);
 
     lift.embedded = false;
-    lift.phi = @(y) sin(y);
-    lift.Dphi = @(y, v) cos(y).*v;
-    lift.Dphit = @(y, u) cos(y).*u;
-    lift.hesshw = @(y, v, w) -sin(y).*w.*v;
+    lift.phi = @(y) scale*sin(y);
+    lift.Dphi = @(y, v) scale*cos(y).*v;
+    lift.Dphit = @(y, u) scale*cos(y).*u;
+    lift.hesshw = @(y, v, w) -scale*sin(y).*w.*v;
 
 end
